0c0511cadd2478996608e50646c9024da12404ed
Kamil Szczygiel <kamil@szczygiel.io>
2019-03-28 08:40:22 +0100

    change main package

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/hashicorp/terraform/plugin"
-	"github.com/terraform-providers/terraform-provider-fastly/fastly"
+	"github.com/Wikia/terraform-provider-fastly/fastly"
 )
 
 func main() {

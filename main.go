2fdf4a7c35044ad2f6e53b54a5d3f1734092191f
Kamil Szczygiel <kamil@szczygiel.io>
2019-04-09 08:58:44 +0200

    fmt

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"github.com/terraform-providers/terraform-provider-fastly/fastly"
 	"github.com/hashicorp/terraform/plugin"
+	"github.com/terraform-providers/terraform-provider-fastly/fastly"
 )
 
 func main() {

45ed31213bad2a42385d0d23d5a3c07b663932c6
Trent Rosenbaum <trent.rosenbaum@opencredo>
2019-06-26 08:59:45 +0100

    Expands the remote state check for the ACL entries.

--- fastly/resource_fastly_service_acl_entries_v1_test.go
+++ fastly/resource_fastly_service_acl_entries_v1_test.go
@@ -64,6 +64,23 @@ func TestAccFastlyServiceAclEntriesV1_create(t *testing.T) {
 	serviceName := fmt.Sprintf("tf-test-%s", acctest.RandString(10))
 	aclName := fmt.Sprintf("ACL %s", acctest.RandString(10))
 
+	expectedEntries := []map[string]interface{}{
+		{
+			"id":      "",
+			"ip":      "127.0.0.1",
+			"subnet":  "24",
+			"negated": false,
+			"comment": "ALC Entry 1",
+		},
+		{
+			"id":      "",
+			"ip":      "192.168.0.1",
+			"subnet":  "16",
+			"negated": false,
+			"comment": "ALC Entry 2",
+		},
+	}
+
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
@@ -73,7 +90,7 @@ func TestAccFastlyServiceAclEntriesV1_create(t *testing.T) {
 				Config: testAccServiceAclEntriesV1Config_create(serviceName, aclName),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckServiceV1Exists("fastly_service_v1.foo", &service),
-					testAccCheckFastlyServiceDictionaryItemsV1RemoteState(&service, serviceName, aclName),
+					testAccCheckFastlyServiceAclEntriesV1RemoteState(&service, serviceName, aclName, expectedEntries),
 					resource.TestCheckResourceAttr("fastly_service_acl_entries_v1.entries", "entry.#", "2"),
 				),
 			},
@@ -81,11 +98,44 @@ func TestAccFastlyServiceAclEntriesV1_create(t *testing.T) {
 	})
 }
 
-func testAccCheckFastlyServiceDictionaryItemsV1RemoteState(service *gofastly.ServiceDetail, serviceName, aclName string) resource.TestCheckFunc {
+func testAccCheckFastlyServiceAclEntriesV1RemoteState(service *gofastly.ServiceDetail, serviceName, aclName string, expectedEntries []map[string]interface{}) resource.TestCheckFunc {
 
 	return func(s *terraform.State) error {
 
-		// TODO check against remote state through api.
+		if service.Name != serviceName {
+			return fmt.Errorf("[ERR] Bad name, expected (%s), got (%s)", serviceName, service.Name)
+		}
+
+		conn := testAccProvider.Meta().(*FastlyClient).conn
+		acl, err := conn.GetACL(&gofastly.GetACLInput{
+			Service: service.ID,
+			Version: service.ActiveVersion.Number,
+			Name:    aclName,
+		})
+
+		if err != nil {
+			return fmt.Errorf("[ERR] Error looking up ACL records for (%s), version (%v): %s", service.Name, service.ActiveVersion.Number, err)
+		}
+
+		aclEntries, err := conn.ListACLEntries(&gofastly.ListACLEntriesInput{
+			Service: service.ID,
+			ACL:     acl.ID,
+		})
+
+		if err != nil {
+			return fmt.Errorf("[ERR] Error looking up ACL entry records for (%s), ACL (%s): %s", service.Name, acl.ID, err)
+		}
+
+		flatAclEntries := flattenAclEntries(aclEntries)
+		// Clear out the id values to allow a deep equal of the other attributes set in terraform.
+		for _, val := range flatAclEntries {
+			val["id"] = ""
+		}
+
+		if !reflect.DeepEqual(flatAclEntries, expectedEntries) {
+			return fmt.Errorf("[ERR] Error matching:\nexpected: %#v\ngot: %#v", expectedEntries, flatAclEntries)
+		}
+
 		return nil
 	}
 }
@@ -127,7 +177,7 @@ resource "fastly_service_v1" "foo" {
 
 	entry {
 		ip = "192.168.0.1"
-		subnet = "24"
+		subnet = "16"
 		negated = false
 		comment = "ALC Entry 2"
 	}

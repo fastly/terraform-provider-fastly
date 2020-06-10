16ddbce370e777270ae1f3545080d568e5f7bd4c
Guy Richardson <guy@Opencredos-MacBook-Pro.local>
2020-06-10 11:35:05 +0100

    Migrated buildDynamicSnippet and flattenDynamicSnippets to block file

--- fastly/resource_fastly_service_v1.go
+++ fastly/resource_fastly_service_v1.go
@@ -4484,42 +4484,6 @@ func buildSnippet(snippetMap interface{}) (*gofastly.CreateSnippetInput, error) 
 	return &opts, nil
 }
 
-func buildDynamicSnippet(dynamicSnippetMap interface{}) (*gofastly.CreateSnippetInput, error) {
-	df := dynamicSnippetMap.(map[string]interface{})
-	opts := gofastly.CreateSnippetInput{
-		Name:     df["name"].(string),
-		Priority: df["priority"].(int),
-		Dynamic:  1,
-	}
-
-	snippetType := strings.ToLower(df["type"].(string))
-	switch snippetType {
-	case "init":
-		opts.Type = gofastly.SnippetTypeInit
-	case "recv":
-		opts.Type = gofastly.SnippetTypeRecv
-	case "hash":
-		opts.Type = gofastly.SnippetTypeHash
-	case "hit":
-		opts.Type = gofastly.SnippetTypeHit
-	case "miss":
-		opts.Type = gofastly.SnippetTypeMiss
-	case "pass":
-		opts.Type = gofastly.SnippetTypePass
-	case "fetch":
-		opts.Type = gofastly.SnippetTypeFetch
-	case "error":
-		opts.Type = gofastly.SnippetTypeError
-	case "deliver":
-		opts.Type = gofastly.SnippetTypeDeliver
-	case "log":
-		opts.Type = gofastly.SnippetTypeLog
-	case "none":
-		opts.Type = gofastly.SnippetTypeNone
-	}
-
-	return &opts, nil
-}
 
 func flattenSnippets(snippetList []*gofastly.Snippet) []map[string]interface{} {
 	var sl []map[string]interface{}
@@ -4550,34 +4514,6 @@ func flattenSnippets(snippetList []*gofastly.Snippet) []map[string]interface{} {
 	return sl
 }
 
-func flattenDynamicSnippets(dynamicSnippetList []*gofastly.Snippet) []map[string]interface{} {
-	var sl []map[string]interface{}
-	for _, dynamicSnippet := range dynamicSnippetList {
-		// Skip non-dynamic snippets
-		if dynamicSnippet.Dynamic == 0 {
-			continue
-		}
-
-		// Convert VCLs to a map for saving to state.
-		dynamicSnippetMap := map[string]interface{}{
-			"snippet_id": dynamicSnippet.ID,
-			"name":       dynamicSnippet.Name,
-			"type":       dynamicSnippet.Type,
-			"priority":   int(dynamicSnippet.Priority),
-		}
-
-		// prune any empty values that come from the default string value in structs
-		for k, v := range dynamicSnippetMap {
-			if v == "" {
-				delete(dynamicSnippetMap, k)
-			}
-		}
-
-		sl = append(sl, dynamicSnippetMap)
-	}
-
-	return sl
-}
 
 func buildDictionary(dictMap interface{}) (*gofastly.CreateDictionaryInput, error) {
 	df := dictMap.(map[string]interface{})

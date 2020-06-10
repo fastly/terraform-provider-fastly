cbf139059e43dc816efc6105bd9f0dafb8643393
Guy Richardson <guy@Opencredos-MacBook-Pro.local>
2020-06-10 08:34:38 +0100

    Migrated condition schema to block file

--- fastly/resource_fastly_service_v1.go
+++ fastly/resource_fastly_service_v1.go
@@ -89,35 +89,7 @@ func resourceServiceV1() *schema.Resource {
 				},
 			},
 
-			"condition": {
-				Type:     schema.TypeSet,
-				Optional: true,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"name": {
-							Type:     schema.TypeString,
-							Required: true,
-						},
-						"statement": {
-							Type:        schema.TypeString,
-							Required:    true,
-							Description: "The statement used to determine if the condition is met",
-						},
-						"priority": {
-							Type:        schema.TypeInt,
-							Optional:    true,
-							Default:     10,
-							Description: "A number used to determine the order in which multiple conditions execute. Lower numbers execute first",
-						},
-						"type": {
-							Type:         schema.TypeString,
-							Required:     true,
-							Description:  "Type of the condition, either `REQUEST`, `RESPONSE`, or `CACHE`",
-							ValidateFunc: validateConditionType(),
-						},
-					},
-				},
-			},
+			"condition": conditionSchema,
 
 			"default_ttl": {
 				Type:        schema.TypeInt,

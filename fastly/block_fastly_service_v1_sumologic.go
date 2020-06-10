41ccfe67ebe7a7c0842b9f1edcfbae91c45d8edd
Guy Richardson <guy@Opencredos-MacBook-Pro.local>
2020-06-10 15:08:59 +0100

    Migrated sumologic schema to block file

---
+++ fastly/block_fastly_service_v1_sumologic.go
@@ -0,0 +1,58 @@
+package fastly
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+var sumologicSchema = &schema.Schema{
+	Type:     schema.TypeSet,
+	Optional: true,
+	Elem: &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			// Required fields
+			"name": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "Unique name to refer to this logging setup",
+			},
+			"url": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "The URL to POST to.",
+			},
+			// Optional fields
+			"format": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Default:     "%h %l %u %t %r %>s",
+				Description: "Apache-style string or VCL variables to use for log formatting",
+			},
+			"format_version": {
+				Type:         schema.TypeInt,
+				Optional:     true,
+				Default:      1,
+				Description:  "The version of the custom logging format used for the configured endpoint. Can be either 1 or 2. (Default: 1)",
+				ValidateFunc: validateLoggingFormatVersion(),
+			},
+			"response_condition": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Default:     "",
+				Description: "Name of a condition to apply this logging.",
+			},
+			"message_type": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				Default:      "classic",
+				Description:  "How the message should be formatted.",
+				ValidateFunc: validateLoggingMessageType(),
+			},
+			"placement": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				Description:  "Where in the generated VCL the logging call should be placed.",
+				ValidateFunc: validateLoggingPlacement(),
+			},
+		},
+	},
+}

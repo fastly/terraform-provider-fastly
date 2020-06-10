66c6b7758416099a8e12149a06882a315a440c46
Guy Richardson <guy@Opencredos-MacBook-Pro.local>
2020-06-10 14:45:51 +0100

    import formatting

---
+++ fastly/block_fastly_service_v1_s3logging.go
@@ -0,0 +1,11 @@
+package fastly
+
+import (
+	"fmt"
+	"log"
+
+	gofastly "github.com/fastly/go-fastly/fastly"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+var s3loggingSchema = &schema.Schema

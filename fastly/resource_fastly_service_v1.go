9c5582f1e5b3358409d6107c827bb5ab783d5247
Guy Richardson <guy@Opencredos-MacBook-Pro.local>
2020-06-10 15:02:50 +0100

    Migrated splunk schema to block file

--- fastly/resource_fastly_service_v1.go
+++ fastly/resource_fastly_service_v1.go
@@ -472,69 +472,7 @@ func resourceServiceV1() *schema.Resource {
 
 			"logentries": logentriesSchema,
 
-			"splunk": {
-				Type:     schema.TypeSet,
-				Optional: true,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						// Required fields
-						"name": {
-							Type:        schema.TypeString,
-							Required:    true,
-							Description: "The unique name of the Splunk logging endpoint",
-						},
-						"url": {
-							Type:        schema.TypeString,
-							Required:    true,
-							Description: "The Splunk URL to stream logs to",
-						},
-						"token": {
-							Type:        schema.TypeString,
-							Required:    true,
-							DefaultFunc: schema.EnvDefaultFunc("FASTLY_SPLUNK_TOKEN", ""),
-							Description: "The Splunk token to be used for authentication",
-							Sensitive:   true,
-						},
-						// Optional fields
-						"format": {
-							Type:        schema.TypeString,
-							Optional:    true,
-							Default:     "%h %l %u %t \"%r\" %>s %b",
-							Description: "Apache-style string or VCL variables to use for log formatting (default: `%h %l %u %t \"%r\" %>s %b`)",
-						},
-						"format_version": {
-							Type:         schema.TypeInt,
-							Optional:     true,
-							Default:      2,
-							Description:  "The version of the custom logging format used for the configured endpoint. Can be either 1 or 2. (default: 2)",
-							ValidateFunc: validateLoggingFormatVersion(),
-						},
-						"placement": {
-							Type:         schema.TypeString,
-							Optional:     true,
-							Description:  "Where in the generated VCL the logging call should be placed",
-							ValidateFunc: validateLoggingPlacement(),
-						},
-						"response_condition": {
-							Type:        schema.TypeString,
-							Optional:    true,
-							Description: "The name of the condition to apply",
-						},
-						"tls_hostname": {
-							Type:        schema.TypeString,
-							Optional:    true,
-							Description: "The hostname used to verify the server's certificate. It can either be the Common Name or a Subject Alternative Name (SAN).",
-						},
-						"tls_ca_cert": {
-							Type:        schema.TypeString,
-							Optional:    true,
-							DefaultFunc: schema.EnvDefaultFunc("FASTLY_SPLUNK_CA_CERT", ""),
-							Description: "A secure certificate to authenticate the server with. Must be in PEM format. You can provide this certificate via an environment variable, `FASTLY_SPLUNK_CA_CERT`.",
-						},
-					},
-				},
-			},
-
+			"splunk":             splunkSchema,
 			"blobstoragelogging": blobstorageloggingSchema,
 
 			"httpslogging":          httpsloggingSchema,

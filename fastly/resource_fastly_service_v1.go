997d61c7b7e9aaf77a6836a499c4607f0350f143
Guy Richardson <guy@Opencredos-MacBook-Pro.local>
2020-06-10 13:09:35 +0100

    Migrated gzip schema to block file

--- fastly/resource_fastly_service_v1.go
+++ fastly/resource_fastly_service_v1.go
@@ -385,39 +385,7 @@ func resourceServiceV1() *schema.Resource {
 				},
 			},
 
-			"gzip": {
-				Type:     schema.TypeSet,
-				Optional: true,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						// required fields
-						"name": {
-							Type:        schema.TypeString,
-							Required:    true,
-							Description: "A name to refer to this gzip condition",
-						},
-						// optional fields
-						"content_types": {
-							Type:        schema.TypeSet,
-							Optional:    true,
-							Description: "Content types to apply automatic gzip to",
-							Elem:        &schema.Schema{Type: schema.TypeString},
-						},
-						"extensions": {
-							Type:        schema.TypeSet,
-							Optional:    true,
-							Description: "File extensions to apply automatic gzip to. Do not include '.'",
-							Elem:        &schema.Schema{Type: schema.TypeString},
-						},
-						"cache_condition": {
-							Type:        schema.TypeString,
-							Optional:    true,
-							Default:     "",
-							Description: "Name of a condition controlling when this gzip configuration applies.",
-						},
-					},
-				},
-			},
+			"gzip": gzipSchema,
 
 			"header": {
 				Type:     schema.TypeSet,

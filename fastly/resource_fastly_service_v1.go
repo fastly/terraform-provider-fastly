0a2122241df353bb8201cd29aabbe8b3f10ce8a5
Guy Richardson <guy@Opencredos-MacBook-Pro.local>
2020-06-10 15:05:31 +0100

    Migrated readSplunk to block file

--- fastly/resource_fastly_service_v1.go
+++ fastly/resource_fastly_service_v1.go
@@ -1398,20 +1398,8 @@ func resourceServiceV1Read(d *schema.ResourceData, meta interface{}) error {
 		}
 
 		// refresh Splunk Logging
-		log.Printf("[DEBUG] Refreshing Splunks for (%s)", d.Id())
-		splunkList, err := conn.ListSplunks(&gofastly.ListSplunksInput{
-			Service: d.Id(),
-			Version: s.ActiveVersion.Number,
-		})
-
-		if err != nil {
-			return fmt.Errorf("[ERR] Error looking up Splunks for (%s), version (%v): %s", d.Id(), s.ActiveVersion.Number, err)
-		}
-
-		spl := flattenSplunks(splunkList)
-
-		if err := d.Set("splunk", spl); err != nil {
-			log.Printf("[WARN] Error setting Splunks for (%s): %s", d.Id(), err)
+		if err := readSplunk(conn, d, s); err != nil {
+			return err
 		}
 
 		// refresh Blob Storage Logging

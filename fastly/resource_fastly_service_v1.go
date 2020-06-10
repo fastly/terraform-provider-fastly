41dafa041b04f87dd10bf0987db8191897da7301
Guy Richardson <guy@Opencredos-MacBook-Pro.local>
2020-06-10 15:08:59 +0100

    Migrated sumologic schema to block file

--- fastly/resource_fastly_service_v1.go
+++ fastly/resource_fastly_service_v1.go
@@ -313,64 +313,11 @@ func resourceServiceV1() *schema.Resource {
 				},
 			},
 
-			"gzip":       gzipSchema,
-			"header":     headerSchema,
-			"s3logging":  s3loggingSchema,
-			"papertrail": papertrailSchema,
-
-			"sumologic": {
-				Type:     schema.TypeSet,
-				Optional: true,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						// Required fields
-						"name": {
-							Type:        schema.TypeString,
-							Required:    true,
-							Description: "Unique name to refer to this logging setup",
-						},
-						"url": {
-							Type:        schema.TypeString,
-							Required:    true,
-							Description: "The URL to POST to.",
-						},
-						// Optional fields
-						"format": {
-							Type:        schema.TypeString,
-							Optional:    true,
-							Default:     "%h %l %u %t %r %>s",
-							Description: "Apache-style string or VCL variables to use for log formatting",
-						},
-						"format_version": {
-							Type:         schema.TypeInt,
-							Optional:     true,
-							Default:      1,
-							Description:  "The version of the custom logging format used for the configured endpoint. Can be either 1 or 2. (Default: 1)",
-							ValidateFunc: validateLoggingFormatVersion(),
-						},
-						"response_condition": {
-							Type:        schema.TypeString,
-							Optional:    true,
-							Default:     "",
-							Description: "Name of a condition to apply this logging.",
-						},
-						"message_type": {
-							Type:         schema.TypeString,
-							Optional:     true,
-							Default:      "classic",
-							Description:  "How the message should be formatted.",
-							ValidateFunc: validateLoggingMessageType(),
-						},
-						"placement": {
-							Type:         schema.TypeString,
-							Optional:     true,
-							Description:  "Where in the generated VCL the logging call should be placed.",
-							ValidateFunc: validateLoggingPlacement(),
-						},
-					},
-				},
-			},
-
+			"gzip":            gzipSchema,
+			"header":          headerSchema,
+			"s3logging":       s3loggingSchema,
+			"papertrail":      papertrailSchema,
+			"sumologic":       sumologicSchema,
 			"gcslogging":      gcsloggingSchema,
 			"bigquerylogging": bigqueryloggingSchema,
 

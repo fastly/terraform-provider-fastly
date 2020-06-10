d5de8b37fced9b5ed2a136428c7a57f81d2a9ff5
Guy Richardson <guy@Opencredos-MacBook-Pro.local>
2020-06-10 15:30:41 +0100

    Migrated flattenVCLs and validateVCL to block file

--- fastly/block_fastly_service_v1_vcl.go
+++ fastly/block_fastly_service_v1_vcl.go
@@ -1,6 +1,7 @@
 package fastly
 
 import (
+	"errors"
 	"fmt"
 	gofastly "github.com/fastly/go-fastly/fastly"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
@@ -118,6 +119,58 @@ func readVCL(conn *gofastly.Client, d *schema.ResourceData, s *gofastly.ServiceD
 
 	if err := d.Set("vcl", vl); err != nil {
 		log.Printf("[WARN] Error setting VCLs for (%s): %s", d.Id(), err)
+	}
+	return nil
+}
+
+
+func flattenVCLs(vclList []*gofastly.VCL) []map[string]interface{} {
+	var vl []map[string]interface{}
+	for _, vcl := range vclList {
+		// Convert VCLs to a map for saving to state.
+		vclMap := map[string]interface{}{
+			"name":    vcl.Name,
+			"content": vcl.Content,
+			"main":    vcl.Main,
+		}
+
+		// prune any empty values that come from the default string value in structs
+		for k, v := range vclMap {
+			if v == "" {
+				delete(vclMap, k)
+			}
+		}
+
+		vl = append(vl, vclMap)
+	}
+
+	return vl
+}
+
+
+
+func validateVCLs(d *schema.ResourceData) error {
+	// TODO: this would be nice to move into a resource/collection validation function, once that is available
+	// (see https://github.com/hashicorp/terraform/pull/4348 and https://github.com/hashicorp/terraform/pull/6508)
+	vcls, exists := d.GetOk("vcl")
+	if !exists {
+		return nil
+	}
+
+	numberOfMainVCLs, numberOfIncludeVCLs := 0, 0
+	for _, vclElem := range vcls.(*schema.Set).List() {
+		vcl := vclElem.(map[string]interface{})
+		if mainVal, hasMain := vcl["main"]; hasMain && mainVal.(bool) {
+			numberOfMainVCLs++
+		} else {
+			numberOfIncludeVCLs++
+		}
+	}
+	if numberOfMainVCLs == 0 && numberOfIncludeVCLs > 0 {
+		return errors.New("if you include VCL configurations, one of them should have main = true")
+	}
+	if numberOfMainVCLs > 1 {
+		return errors.New("you cannot have more than one VCL configuration with main = true")
 	}
 	return nil
 }

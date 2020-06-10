9291f7e00459d10c43a835c9b6a6cdc38f05a053
Guy Richardson <guy@Opencredos-MacBook-Pro.local>
2020-06-10 15:28:48 +0100

    Migrated processVCL to block file

--- fastly/resource_fastly_service_v1.go
+++ fastly/resource_fastly_service_v1.go
@@ -790,71 +790,8 @@ func resourceServiceV1Update(d *schema.ResourceData, meta interface{}) error {
 
 		// Find differences in VCLs
 		if d.HasChange("vcl") {
-			// Note: as above with Gzip and S3 logging, we don't utilize the PUT
-			// endpoint to update a VCL, we simply destroy it and create a new one.
-			oldVCLVal, newVCLVal := d.GetChange("vcl")
-			if oldVCLVal == nil {
-				oldVCLVal = new(schema.Set)
-			}
-			if newVCLVal == nil {
-				newVCLVal = new(schema.Set)
-			}
-
-			oldVCLSet := oldVCLVal.(*schema.Set)
-			newVCLSet := newVCLVal.(*schema.Set)
-
-			remove := oldVCLSet.Difference(newVCLSet).List()
-			add := newVCLSet.Difference(oldVCLSet).List()
-
-			// Delete removed VCL configurations
-			for _, dRaw := range remove {
-				df := dRaw.(map[string]interface{})
-				opts := gofastly.DeleteVCLInput{
-					Service: d.Id(),
-					Version: latestVersion,
-					Name:    df["name"].(string),
-				}
-
-				log.Printf("[DEBUG] Fastly VCL Removal opts: %#v", opts)
-				err := conn.DeleteVCL(&opts)
-				if errRes, ok := err.(*gofastly.HTTPError); ok {
-					if errRes.StatusCode != 404 {
-						return err
-					}
-				} else if err != nil {
-					return err
-				}
-			}
-			// POST new VCL configurations
-			for _, dRaw := range add {
-				df := dRaw.(map[string]interface{})
-				opts := gofastly.CreateVCLInput{
-					Service: d.Id(),
-					Version: latestVersion,
-					Name:    df["name"].(string),
-					Content: df["content"].(string),
-				}
-
-				log.Printf("[DEBUG] Fastly VCL Addition opts: %#v", opts)
-				_, err := conn.CreateVCL(&opts)
-				if err != nil {
-					return err
-				}
-
-				// if this new VCL is the main
-				if df["main"].(bool) {
-					opts := gofastly.ActivateVCLInput{
-						Service: d.Id(),
-						Version: latestVersion,
-						Name:    df["name"].(string),
-					}
-					log.Printf("[DEBUG] Fastly VCL activation opts: %#v", opts)
-					_, err := conn.ActivateVCL(&opts)
-					if err != nil {
-						return err
-					}
-
-				}
+			if err := processVCL(d, conn, latestVersion); err != nil {
+				return err
 			}
 		}
 

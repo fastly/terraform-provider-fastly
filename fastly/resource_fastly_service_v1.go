d73539c37bc73cb38be4ce70c2651857ee49a781
Guy Richardson <guy@Opencredos-MacBook-Pro.local>
2020-06-10 14:16:29 +0100

    Migrated papertrail schema to block file

--- fastly/resource_fastly_service_v1.go
+++ fastly/resource_fastly_service_v1.go
@@ -428,49 +428,7 @@ func resourceServiceV1() *schema.Resource {
 				},
 			},
 
-			"papertrail": {
-				Type:     schema.TypeSet,
-				Optional: true,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						// Required fields
-						"name": {
-							Type:        schema.TypeString,
-							Required:    true,
-							Description: "Unique name to refer to this logging setup",
-						},
-						"address": {
-							Type:        schema.TypeString,
-							Required:    true,
-							Description: "The address of the papertrail service",
-						},
-						"port": {
-							Type:        schema.TypeInt,
-							Required:    true,
-							Description: "The port of the papertrail service",
-						},
-						// Optional fields
-						"format": {
-							Type:        schema.TypeString,
-							Optional:    true,
-							Default:     "%h %l %u %t %r %>s",
-							Description: "Apache-style string or VCL variables to use for log formatting",
-						},
-						"response_condition": {
-							Type:        schema.TypeString,
-							Optional:    true,
-							Default:     "",
-							Description: "Name of a condition to apply this logging",
-						},
-						"placement": {
-							Type:         schema.TypeString,
-							Optional:     true,
-							Description:  "Where in the generated VCL the logging call should be placed.",
-							ValidateFunc: validateLoggingPlacement(),
-						},
-					},
-				},
-			},
+			"papertrail": papertrailSchema,
 
 			"sumologic": {
 				Type:     schema.TypeSet,

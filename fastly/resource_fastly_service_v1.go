8d0f99fe4927226831ed333b8baca781444c97ad
Guy Richardson <guy@Opencredos-MacBook-Pro.local>
2020-06-10 08:36:36 +0100

    Migrated processCondition to block file

--- fastly/resource_fastly_service_v1.go
+++ fastly/resource_fastly_service_v1.go
@@ -1491,64 +1491,8 @@ func resourceServiceV1Update(d *schema.ResourceData, meta interface{}) error {
 
 		// Find difference in Conditions
 		if d.HasChange("condition") {
-			// Note: we don't utilize the PUT endpoint to update these objects, we simply
-			// destroy any that have changed, and create new ones with the updated
-			// values. This is how Terraform works with nested sub resources, we only
-			// get the full diff not a partial set item diff. Because this is done
-			// on a new version of the Fastly Service configuration, this is considered safe
-
-			oc, nc := d.GetChange("condition")
-			if oc == nil {
-				oc = new(schema.Set)
-			}
-			if nc == nil {
-				nc = new(schema.Set)
-			}
-
-			ocs := oc.(*schema.Set)
-			ncs := nc.(*schema.Set)
-			removeConditions := ocs.Difference(ncs).List()
-			addConditions := ncs.Difference(ocs).List()
-
-			// DELETE old Conditions
-			for _, cRaw := range removeConditions {
-				cf := cRaw.(map[string]interface{})
-				opts := gofastly.DeleteConditionInput{
-					Service: d.Id(),
-					Version: latestVersion,
-					Name:    cf["name"].(string),
-				}
-
-				log.Printf("[DEBUG] Fastly Conditions Removal opts: %#v", opts)
-				err := conn.DeleteCondition(&opts)
-				if errRes, ok := err.(*gofastly.HTTPError); ok {
-					if errRes.StatusCode != 404 {
-						return err
-					}
-				} else if err != nil {
-					return err
-				}
-			}
-
-			// POST new Conditions
-			for _, cRaw := range addConditions {
-				cf := cRaw.(map[string]interface{})
-				opts := gofastly.CreateConditionInput{
-					Service: d.Id(),
-					Version: latestVersion,
-					Name:    cf["name"].(string),
-					Type:    cf["type"].(string),
-					// need to trim leading/tailing spaces, incase the config has HEREDOC
-					// formatting and contains a trailing new line
-					Statement: strings.TrimSpace(cf["statement"].(string)),
-					Priority:  cf["priority"].(int),
-				}
-
-				log.Printf("[DEBUG] Create Conditions Opts: %#v", opts)
-				_, err := conn.CreateCondition(&opts)
-				if err != nil {
-					return err
-				}
+			if err := processCondition(d, conn, latestVersion); err != nil {
+				return err
 			}
 		}
 

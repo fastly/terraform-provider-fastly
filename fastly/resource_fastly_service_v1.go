e0b44441bf2b532ae0f253809171197b4940dd51
Guy Richardson <guy@Opencredos-MacBook-Pro.local>
2020-06-10 15:58:19 +0100

    Migrated processDomain to block file

--- fastly/resource_fastly_service_v1.go
+++ fastly/resource_fastly_service_v1.go
@@ -300,58 +300,8 @@ func resourceServiceV1Update(d *schema.ResourceData, meta interface{}) error {
 
 		// Find differences in domains
 		if d.HasChange("domain") {
-			od, nd := d.GetChange("domain")
-			if od == nil {
-				od = new(schema.Set)
-			}
-			if nd == nil {
-				nd = new(schema.Set)
-			}
-
-			ods := od.(*schema.Set)
-			nds := nd.(*schema.Set)
-
-			remove := ods.Difference(nds).List()
-			add := nds.Difference(ods).List()
-
-			// Delete removed domains
-			for _, dRaw := range remove {
-				df := dRaw.(map[string]interface{})
-				opts := gofastly.DeleteDomainInput{
-					Service: d.Id(),
-					Version: latestVersion,
-					Name:    df["name"].(string),
-				}
-
-				log.Printf("[DEBUG] Fastly Domain removal opts: %#v", opts)
-				err := conn.DeleteDomain(&opts)
-				if errRes, ok := err.(*gofastly.HTTPError); ok {
-					if errRes.StatusCode != 404 {
-						return err
-					}
-				} else if err != nil {
-					return err
-				}
-			}
-
-			// POST new Domains
-			for _, dRaw := range add {
-				df := dRaw.(map[string]interface{})
-				opts := gofastly.CreateDomainInput{
-					Service: d.Id(),
-					Version: latestVersion,
-					Name:    df["name"].(string),
-				}
-
-				if v, ok := df["comment"]; ok {
-					opts.Comment = v.(string)
-				}
-
-				log.Printf("[DEBUG] Fastly Domain Addition opts: %#v", opts)
-				_, err := conn.CreateDomain(&opts)
-				if err != nil {
-					return err
-				}
+			if err := processDomain(d, conn, latestVersion); err != nil {
+				return err
 			}
 		}
 

c0aad5ef617ab56eac9469d1a485a662238b9460
Guy Richardson <guy@Opencredos-MacBook-Pro.local>
2020-06-10 15:58:19 +0100

    Migrated processDomain to block file

--- fastly/block_fastly_service_v1_domain.go
+++ fastly/block_fastly_service_v1_domain.go
@@ -1,7 +1,9 @@
 package fastly
 
 import (
+	gofastly "github.com/fastly/go-fastly/fastly"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"log"
 )
 
 var domainSchema = &schema.Schema{
@@ -21,4 +23,61 @@ var domainSchema = &schema.Schema{
 			},
 		},
 	},
+}
+
+func processDomain(d *schema.ResourceData, conn *gofastly.Client, latestVersion int) error {
+	od, nd := d.GetChange("domain")
+	if od == nil {
+		od = new(schema.Set)
+	}
+	if nd == nil {
+		nd = new(schema.Set)
+	}
+
+	ods := od.(*schema.Set)
+	nds := nd.(*schema.Set)
+
+	remove := ods.Difference(nds).List()
+	add := nds.Difference(ods).List()
+
+	// Delete removed domains
+	for _, dRaw := range remove {
+		df := dRaw.(map[string]interface{})
+		opts := gofastly.DeleteDomainInput{
+			Service: d.Id(),
+			Version: latestVersion,
+			Name:    df["name"].(string),
+		}
+
+		log.Printf("[DEBUG] Fastly Domain removal opts: %#v", opts)
+		err := conn.DeleteDomain(&opts)
+		if errRes, ok := err.(*gofastly.HTTPError); ok {
+			if errRes.StatusCode != 404 {
+				return err
+			}
+		} else if err != nil {
+			return err
+		}
+	}
+
+	// POST new Domains
+	for _, dRaw := range add {
+		df := dRaw.(map[string]interface{})
+		opts := gofastly.CreateDomainInput{
+			Service: d.Id(),
+			Version: latestVersion,
+			Name:    df["name"].(string),
+		}
+
+		if v, ok := df["comment"]; ok {
+			opts.Comment = v.(string)
+		}
+
+		log.Printf("[DEBUG] Fastly Domain Addition opts: %#v", opts)
+		_, err := conn.CreateDomain(&opts)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }

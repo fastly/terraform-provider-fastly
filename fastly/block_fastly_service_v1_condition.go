320750dfcfa56c41caaddda23e55016bdd94f98f
Guy Richardson <guy@Opencredos-MacBook-Pro.local>
2020-06-10 08:36:36 +0100

    Migrated processCondition to block file

--- fastly/block_fastly_service_v1_condition.go
+++ fastly/block_fastly_service_v1_condition.go
@@ -1,6 +1,11 @@
 package fastly
 
-import "github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+import (
+	gofastly "github.com/fastly/go-fastly/fastly"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"log"
+	"strings"
+)
 
 var conditionSchema = &schema.Schema{
 	Type:     schema.TypeSet,
@@ -31,3 +36,69 @@ var conditionSchema = &schema.Schema{
 		},
 	},
 }
+
+
+func processCondition(d *schema.ResourceData, conn *gofastly.Client, latestVersion int) error {
+	// Note: we don't utilize the PUT endpoint to update these objects, we simply
+	// destroy any that have changed, and create new ones with the updated
+	// values. This is how Terraform works with nested sub resources, we only
+	// get the full diff not a partial set item diff. Because this is done
+	// on a new version of the Fastly Service configuration, this is considered safe
+
+	oc, nc := d.GetChange("condition")
+	if oc == nil {
+		oc = new(schema.Set)
+	}
+	if nc == nil {
+		nc = new(schema.Set)
+	}
+
+	ocs := oc.(*schema.Set)
+	ncs := nc.(*schema.Set)
+	removeConditions := ocs.Difference(ncs).List()
+	addConditions := ncs.Difference(ocs).List()
+
+	// DELETE old Conditions
+	for _, cRaw := range removeConditions {
+		cf := cRaw.(map[string]interface{})
+		opts := gofastly.DeleteConditionInput{
+			Service: d.Id(),
+			Version: latestVersion,
+			Name:    cf["name"].(string),
+		}
+
+		log.Printf("[DEBUG] Fastly Conditions Removal opts: %#v", opts)
+		err := conn.DeleteCondition(&opts)
+		if errRes, ok := err.(*gofastly.HTTPError); ok {
+			if errRes.StatusCode != 404 {
+				return err
+			}
+		} else if err != nil {
+			return err
+		}
+	}
+
+	// POST new Conditions
+	for _, cRaw := range addConditions {
+		cf := cRaw.(map[string]interface{})
+		opts := gofastly.CreateConditionInput{
+			Service: d.Id(),
+			Version: latestVersion,
+			Name:    cf["name"].(string),
+			Type:    cf["type"].(string),
+			// need to trim leading/tailing spaces, incase the config has HEREDOC
+			// formatting and contains a trailing new line
+			Statement: strings.TrimSpace(cf["statement"].(string)),
+			Priority:  cf["priority"].(int),
+		}
+
+		log.Printf("[DEBUG] Create Conditions Opts: %#v", opts)
+		_, err := conn.CreateCondition(&opts)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+

06d7ee667982cf5feb5858383ee68698af0213df
Guy Richardson <guy@Opencredos-MacBook-Pro.local>
2020-06-11 17:20:37 +0100

    Migrated schema for healthcheck to Register

--- fastly/block_fastly_service_v1_healthcheck.go
+++ fastly/block_fastly_service_v1_healthcheck.go
@@ -15,85 +15,11 @@ type HealthCheckServiceAttributeHandler struct {
 func NewServiceHealthCheck() ServiceAttributeDefinition {
 	return &HealthCheckServiceAttributeHandler{
 		&DefaultServiceAttributeHandler{
-			schema: healthcheckSchema,
 			key:    "healthcheck",
 		},
 	}
 }
 
-var healthcheckSchema = &schema.Schema{
-	Type:     schema.TypeSet,
-	Optional: true,
-	Elem: &schema.Resource{
-		Schema: map[string]*schema.Schema{
-			// required fields
-			"name": {
-				Type:        schema.TypeString,
-				Required:    true,
-				Description: "A name to refer to this healthcheck",
-			},
-			"host": {
-				Type:        schema.TypeString,
-				Required:    true,
-				Description: "Which host to check",
-			},
-			"path": {
-				Type:        schema.TypeString,
-				Required:    true,
-				Description: "The path to check",
-			},
-			// optional fields
-			"check_interval": {
-				Type:        schema.TypeInt,
-				Optional:    true,
-				Default:     5000,
-				Description: "How often to run the healthcheck in milliseconds",
-			},
-			"expected_response": {
-				Type:        schema.TypeInt,
-				Optional:    true,
-				Default:     200,
-				Description: "The status code expected from the host",
-			},
-			"http_version": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Default:     "1.1",
-				Description: "Whether to use version 1.0 or 1.1 HTTP",
-			},
-			"initial": {
-				Type:        schema.TypeInt,
-				Optional:    true,
-				Default:     2,
-				Description: "When loading a config, the initial number of probes to be seen as OK",
-			},
-			"method": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Default:     "HEAD",
-				Description: "Which HTTP method to use",
-			},
-			"threshold": {
-				Type:        schema.TypeInt,
-				Optional:    true,
-				Default:     3,
-				Description: "How many healthchecks must succeed to be considered healthy",
-			},
-			"timeout": {
-				Type:        schema.TypeInt,
-				Optional:    true,
-				Default:     500,
-				Description: "Timeout in milliseconds",
-			},
-			"window": {
-				Type:        schema.TypeInt,
-				Optional:    true,
-				Default:     5,
-				Description: "The number of most recent healthcheck queries to keep for this healthcheck",
-			},
-		},
-	},
-}
 
 func (h *HealthCheckServiceAttributeHandler) Process(d *schema.ResourceData, latestVersion int, conn *gofastly.Client) error {
 	oh, nh := d.GetChange("healthcheck")
@@ -178,6 +104,84 @@ func (h *HealthCheckServiceAttributeHandler) Read(d *schema.ResourceData, s *gof
 
 	return nil
 }
+
+func (h *HealthCheckServiceAttributeHandler) Register(s *schema.Resource) error {
+	s.Schema[h.GetKey()] = &schema.Schema{
+		Type:     schema.TypeSet,
+		Optional: true,
+		Elem: &schema.Resource{
+			Schema: map[string]*schema.Schema{
+				// required fields
+				"name": {
+					Type:        schema.TypeString,
+					Required:    true,
+					Description: "A name to refer to this healthcheck",
+				},
+				"host": {
+					Type:        schema.TypeString,
+					Required:    true,
+					Description: "Which host to check",
+				},
+				"path": {
+					Type:        schema.TypeString,
+					Required:    true,
+					Description: "The path to check",
+				},
+				// optional fields
+				"check_interval": {
+					Type:        schema.TypeInt,
+					Optional:    true,
+					Default:     5000,
+					Description: "How often to run the healthcheck in milliseconds",
+				},
+				"expected_response": {
+					Type:        schema.TypeInt,
+					Optional:    true,
+					Default:     200,
+					Description: "The status code expected from the host",
+				},
+				"http_version": {
+					Type:        schema.TypeString,
+					Optional:    true,
+					Default:     "1.1",
+					Description: "Whether to use version 1.0 or 1.1 HTTP",
+				},
+				"initial": {
+					Type:        schema.TypeInt,
+					Optional:    true,
+					Default:     2,
+					Description: "When loading a config, the initial number of probes to be seen as OK",
+				},
+				"method": {
+					Type:        schema.TypeString,
+					Optional:    true,
+					Default:     "HEAD",
+					Description: "Which HTTP method to use",
+				},
+				"threshold": {
+					Type:        schema.TypeInt,
+					Optional:    true,
+					Default:     3,
+					Description: "How many healthchecks must succeed to be considered healthy",
+				},
+				"timeout": {
+					Type:        schema.TypeInt,
+					Optional:    true,
+					Default:     500,
+					Description: "Timeout in milliseconds",
+				},
+				"window": {
+					Type:        schema.TypeInt,
+					Optional:    true,
+					Default:     5,
+					Description: "The number of most recent healthcheck queries to keep for this healthcheck",
+				},
+			},
+		},
+	}
+	return nil
+}
+
 
 func flattenHealthchecks(healthcheckList []*gofastly.HealthCheck) []map[string]interface{} {
 	var hl []map[string]interface{}

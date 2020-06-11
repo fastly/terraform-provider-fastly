362b05628ec070b749b700db744028388efb09dd
Guy Richardson <guy@Opencredos-MacBook-Pro.local>
2020-06-11 17:19:45 +0100

    Migrated schema for header to Register

--- fastly/block_fastly_service_v1_header.go
+++ fastly/block_fastly_service_v1_header.go
@@ -16,92 +16,11 @@ type HeaderServiceAttributeHandler struct {
 func NewServiceHeader() ServiceAttributeDefinition {
 	return &HeaderServiceAttributeHandler{
 		&DefaultServiceAttributeHandler{
-			schema: headerSchema,
 			key:    "header",
 		},
 	}
 }
 
-var headerSchema = &schema.Schema{
-	Type:     schema.TypeSet,
-	Optional: true,
-	Elem: &schema.Resource{
-		Schema: map[string]*schema.Schema{
-			// required fields
-			"name": {
-				Type:        schema.TypeString,
-				Required:    true,
-				Description: "A name to refer to this Header object",
-			},
-			"action": {
-				Type:         schema.TypeString,
-				Required:     true,
-				Description:  "One of set, append, delete, regex, or regex_repeat",
-				ValidateFunc: validateHeaderAction(),
-			},
-			"type": {
-				Type:         schema.TypeString,
-				Required:     true,
-				Description:  "Type to manipulate: request, fetch, cache, response",
-				ValidateFunc: validateHeaderType(),
-			},
-			"destination": {
-				Type:        schema.TypeString,
-				Required:    true,
-				Description: "Header this affects",
-			},
-			// Optional fields, defaults where they exist
-			"ignore_if_set": {
-				Type:        schema.TypeBool,
-				Optional:    true,
-				Default:     false,
-				Description: "Don't add the header if it is already. (Only applies to 'set' action.). Default `false`",
-			},
-			"source": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Computed:    true,
-				Description: "Variable to be used as a source for the header content (Does not apply to 'delete' action.)",
-			},
-			"regex": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Computed:    true,
-				Description: "Regular expression to use (Only applies to 'regex' and 'regex_repeat' actions.)",
-			},
-			"substitution": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Computed:    true,
-				Description: "Value to substitute in place of regular expression. (Only applies to 'regex' and 'regex_repeat'.)",
-			},
-			"priority": {
-				Type:        schema.TypeInt,
-				Optional:    true,
-				Default:     100,
-				Description: "Lower priorities execute first. (Default: 100.)",
-			},
-			"request_condition": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Default:     "",
-				Description: "Optional name of a request condition to apply.",
-			},
-			"cache_condition": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Default:     "",
-				Description: "Optional name of a cache condition to apply.",
-			},
-			"response_condition": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Default:     "",
-				Description: "Optional name of a response condition to apply.",
-			},
-		},
-	},
-}
 
 func (h *HeaderServiceAttributeHandler) Process(d *schema.ResourceData, latestVersion int, conn *gofastly.Client) error {
 	oh, nh := d.GetChange("header")
@@ -175,6 +94,90 @@ func (h *HeaderServiceAttributeHandler) Read(d *schema.ResourceData, s *gofastly
 		log.Printf("[WARN] Error setting Headers for (%s): %s", d.Id(), err)
 	}
 
+	return nil
+}
+
+func (h *HeaderServiceAttributeHandler) Register(s *schema.Resource) error {
+	s.Schema[h.GetKey()] = &schema.Schema{
+		Type:     schema.TypeSet,
+		Optional: true,
+		Elem: &schema.Resource{
+			Schema: map[string]*schema.Schema{
+				// required fields
+				"name": {
+					Type:        schema.TypeString,
+					Required:    true,
+					Description: "A name to refer to this Header object",
+				},
+				"action": {
+					Type:         schema.TypeString,
+					Required:     true,
+					Description:  "One of set, append, delete, regex, or regex_repeat",
+					ValidateFunc: validateHeaderAction(),
+				},
+				"type": {
+					Type:         schema.TypeString,
+					Required:     true,
+					Description:  "Type to manipulate: request, fetch, cache, response",
+					ValidateFunc: validateHeaderType(),
+				},
+				"destination": {
+					Type:        schema.TypeString,
+					Required:    true,
+					Description: "Header this affects",
+				},
+				// Optional fields, defaults where they exist
+				"ignore_if_set": {
+					Type:        schema.TypeBool,
+					Optional:    true,
+					Default:     false,
+					Description: "Don't add the header if it is already. (Only applies to 'set' action.). Default `false`",
+				},
+				"source": {
+					Type:        schema.TypeString,
+					Optional:    true,
+					Computed:    true,
+					Description: "Variable to be used as a source for the header content (Does not apply to 'delete' action.)",
+				},
+				"regex": {
+					Type:        schema.TypeString,
+					Optional:    true,
+					Computed:    true,
+					Description: "Regular expression to use (Only applies to 'regex' and 'regex_repeat' actions.)",
+				},
+				"substitution": {
+					Type:        schema.TypeString,
+					Optional:    true,
+					Computed:    true,
+					Description: "Value to substitute in place of regular expression. (Only applies to 'regex' and 'regex_repeat'.)",
+				},
+				"priority": {
+					Type:        schema.TypeInt,
+					Optional:    true,
+					Default:     100,
+					Description: "Lower priorities execute first. (Default: 100.)",
+				},
+				"request_condition": {
+					Type:        schema.TypeString,
+					Optional:    true,
+					Default:     "",
+					Description: "Optional name of a request condition to apply.",
+				},
+				"cache_condition": {
+					Type:        schema.TypeString,
+					Optional:    true,
+					Default:     "",
+					Description: "Optional name of a cache condition to apply.",
+				},
+				"response_condition": {
+					Type:        schema.TypeString,
+					Optional:    true,
+					Default:     "",
+					Description: "Optional name of a response condition to apply.",
+				},
+			},
+		},
+	}
 	return nil
 }
 

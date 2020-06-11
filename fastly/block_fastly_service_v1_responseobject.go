47d111910db6c1da8ec9b44b2fe3592c8cd1face
Guy Richardson <guy@Opencredos-MacBook-Pro.local>
2020-06-11 17:24:42 +0100

    Migrated schema for responseobject to Register

--- fastly/block_fastly_service_v1_responseobject.go
+++ fastly/block_fastly_service_v1_responseobject.go
@@ -15,63 +15,11 @@ type ResponseObjectServiceAttributeHandler struct {
 func NewServiceResponseObject() ServiceAttributeDefinition {
 	return &ResponseObjectServiceAttributeHandler{
 		&DefaultServiceAttributeHandler{
-			schema: responseobjectSchema,
 			key:    "response_object",
 		},
 	}
 }
 
-var responseobjectSchema = &schema.Schema{
-	Type:     schema.TypeSet,
-	Optional: true,
-	Elem: &schema.Resource{
-		Schema: map[string]*schema.Schema{
-			// Required
-			"name": {
-				Type:        schema.TypeString,
-				Required:    true,
-				Description: "Unique name to refer to this request object",
-			},
-			// Optional fields
-			"status": {
-				Type:        schema.TypeInt,
-				Optional:    true,
-				Default:     200,
-				Description: "The HTTP Status Code of the object",
-			},
-			"response": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Default:     "OK",
-				Description: "The HTTP Response of the object",
-			},
-			"content": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Default:     "",
-				Description: "The content to deliver for the response object",
-			},
-			"content_type": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Default:     "",
-				Description: "The MIME type of the content",
-			},
-			"request_condition": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Default:     "",
-				Description: "Name of the condition to be checked during the request phase to see if the object should be delivered",
-			},
-			"cache_condition": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Default:     "",
-				Description: "Name of the condition checked after we have retrieved an object. If the condition passes then deliver this Request Object instead.",
-			},
-		},
-	},
-}
 
 func (h *ResponseObjectServiceAttributeHandler) Process(d *schema.ResourceData, latestVersion int, conn *gofastly.Client) error {
 	or, nr := d.GetChange("response_object")
@@ -147,6 +95,61 @@ func (h *ResponseObjectServiceAttributeHandler) Read(d *schema.ResourceData, s *
 
 	if err := d.Set("response_object", rol); err != nil {
 		log.Printf("[WARN] Error setting Response Object for (%s): %s", d.Id(), err)
+	}
+	return nil
+}
+
+func (h *ResponseObjectServiceAttributeHandler) Register(s *schema.Resource) error {
+	s.Schema[h.GetKey()] = &schema.Schema{
+		Type:     schema.TypeSet,
+		Optional: true,
+		Elem: &schema.Resource{
+			Schema: map[string]*schema.Schema{
+				// Required
+				"name": {
+					Type:        schema.TypeString,
+					Required:    true,
+					Description: "Unique name to refer to this request object",
+				},
+				// Optional fields
+				"status": {
+					Type:        schema.TypeInt,
+					Optional:    true,
+					Default:     200,
+					Description: "The HTTP Status Code of the object",
+				},
+				"response": {
+					Type:        schema.TypeString,
+					Optional:    true,
+					Default:     "OK",
+					Description: "The HTTP Response of the object",
+				},
+				"content": {
+					Type:        schema.TypeString,
+					Optional:    true,
+					Default:     "",
+					Description: "The content to deliver for the response object",
+				},
+				"content_type": {
+					Type:        schema.TypeString,
+					Optional:    true,
+					Default:     "",
+					Description: "The MIME type of the content",
+				},
+				"request_condition": {
+					Type:        schema.TypeString,
+					Optional:    true,
+					Default:     "",
+					Description: "Name of the condition to be checked during the request phase to see if the object should be delivered",
+				},
+				"cache_condition": {
+					Type:        schema.TypeString,
+					Optional:    true,
+					Default:     "",
+					Description: "Name of the condition checked after we have retrieved an object. If the condition passes then deliver this Request Object instead.",
+				},
+			},
+		},
 	}
 	return nil
 }

6fa72bfcbeb6dee8e8253c86036b812241c7ff79
Guy Richardson <guy@Opencredos-MacBook-Pro.local>
2020-06-11 17:27:45 +0100

    Migrated schema for sumologic to Register

--- fastly/block_fastly_service_v1_sumologic.go
+++ fastly/block_fastly_service_v1_sumologic.go
@@ -15,64 +15,11 @@ type SumologicServiceAttributeHandler struct {
 func NewServiceSumologic() ServiceAttributeDefinition {
 	return &SumologicServiceAttributeHandler{
 		&DefaultServiceAttributeHandler{
-			schema: sumologicSchema,
 			key:    "sumologic",
 		},
 	}
 }
 
-var sumologicSchema = &schema.Schema{
-	Type:     schema.TypeSet,
-	Optional: true,
-	Elem: &schema.Resource{
-		Schema: map[string]*schema.Schema{
-			// Required fields
-			"name": {
-				Type:        schema.TypeString,
-				Required:    true,
-				Description: "Unique name to refer to this logging setup",
-			},
-			"url": {
-				Type:        schema.TypeString,
-				Required:    true,
-				Description: "The URL to POST to.",
-			},
-			// Optional fields
-			"format": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Default:     "%h %l %u %t %r %>s",
-				Description: "Apache-style string or VCL variables to use for log formatting",
-			},
-			"format_version": {
-				Type:         schema.TypeInt,
-				Optional:     true,
-				Default:      1,
-				Description:  "The version of the custom logging format used for the configured endpoint. Can be either 1 or 2. (Default: 1)",
-				ValidateFunc: validateLoggingFormatVersion(),
-			},
-			"response_condition": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Default:     "",
-				Description: "Name of a condition to apply this logging.",
-			},
-			"message_type": {
-				Type:         schema.TypeString,
-				Optional:     true,
-				Default:      "classic",
-				Description:  "How the message should be formatted.",
-				ValidateFunc: validateLoggingMessageType(),
-			},
-			"placement": {
-				Type:         schema.TypeString,
-				Optional:     true,
-				Description:  "Where in the generated VCL the logging call should be placed.",
-				ValidateFunc: validateLoggingPlacement(),
-			},
-		},
-	},
-}
 
 func (h *SumologicServiceAttributeHandler) Process(d *schema.ResourceData, latestVersion int, conn *gofastly.Client) error {
 	os, ns := d.GetChange("sumologic")
@@ -146,6 +93,62 @@ func (h *SumologicServiceAttributeHandler) Read(d *schema.ResourceData, s *gofas
 	sul := flattenSumologics(sumologicList)
 	if err := d.Set("sumologic", sul); err != nil {
 		log.Printf("[WARN] Error setting Sumologic for (%s): %s", d.Id(), err)
+	}
+	return nil
+}
+
+func (h *SumologicServiceAttributeHandler) Register(s *schema.Resource) error {
+	s.Schema[h.GetKey()] = &schema.Schema{
+		Type:     schema.TypeSet,
+		Optional: true,
+		Elem: &schema.Resource{
+			Schema: map[string]*schema.Schema{
+				// Required fields
+				"name": {
+					Type:        schema.TypeString,
+					Required:    true,
+					Description: "Unique name to refer to this logging setup",
+				},
+				"url": {
+					Type:        schema.TypeString,
+					Required:    true,
+					Description: "The URL to POST to.",
+				},
+				// Optional fields
+				"format": {
+					Type:        schema.TypeString,
+					Optional:    true,
+					Default:     "%h %l %u %t %r %>s",
+					Description: "Apache-style string or VCL variables to use for log formatting",
+				},
+				"format_version": {
+					Type:         schema.TypeInt,
+					Optional:     true,
+					Default:      1,
+					Description:  "The version of the custom logging format used for the configured endpoint. Can be either 1 or 2. (Default: 1)",
+					ValidateFunc: validateLoggingFormatVersion(),
+				},
+				"response_condition": {
+					Type:        schema.TypeString,
+					Optional:    true,
+					Default:     "",
+					Description: "Name of a condition to apply this logging.",
+				},
+				"message_type": {
+					Type:         schema.TypeString,
+					Optional:     true,
+					Default:      "classic",
+					Description:  "How the message should be formatted.",
+					ValidateFunc: validateLoggingMessageType(),
+				},
+				"placement": {
+					Type:         schema.TypeString,
+					Optional:     true,
+					Description:  "Where in the generated VCL the logging call should be placed.",
+					ValidateFunc: validateLoggingPlacement(),
+				},
+			},
+		},
 	}
 	return nil
 }

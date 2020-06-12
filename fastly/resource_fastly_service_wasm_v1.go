e1da0b037c982c98e7297849c7518a723169ba90
Guy Richardson <guy@Opencredos-MacBook-Pro.local>
2020-06-12 17:26:41 +0100

    Added initial WASM configuration

---
+++ fastly/resource_fastly_service_wasm_v1.go
@@ -0,0 +1,34 @@
+package fastly
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+// Ordering is important - stored is processing order
+// Conditions need to be updated first, as they can be referenced by other
+// configuration objects (Backends, Request Headers, etc)
+
+var wasmService = &BaseServiceDefinition{
+	Type: "wasm",
+	Attributes: []ServiceAttributeDefinition{
+		NewServiceDomain(),
+		NewServiceHealthCheck(),
+		NewServiceBackend(),
+		NewServiceS3Logging(),
+		NewServicePaperTrail(),
+		NewServiceSumologic(),
+		NewServiceGCSLogging(),
+		NewServiceBigQueryLogging(),
+		NewServiceSyslog(),
+		NewServiceLogEntries(),
+		NewServiceSplunk(),
+		NewServiceBlobStorageLogging(),
+		NewServiceHTTPSLogging(),
+		NewServiceElasticSearch(),
+		NewServiceFTP(),
+	},
+}
+
+func resourceServiceWASMV1() *schema.Resource {
+	return resourceService(wasmService)
+}

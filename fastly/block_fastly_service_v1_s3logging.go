fc0122fb8e0728e5074b4e11f98d3216bc24c022
Colton J. McCurdy <cmccurdy@fastly.com>
2020-06-24 11:48:45 -0400

    fastly_service_v1/s3logging: use trimSpaceStateFunc
    Signed-off-by: Colton J. McCurdy <cmccurdy@fastly.com>

--- fastly/block_fastly_service_v1_s3logging.go
+++ fastly/block_fastly_service_v1_s3logging.go
@@ -49,6 +49,21 @@ func (h *S3LoggingServiceAttributeHandler) Process(d *schema.ResourceData, lates
 	// POST new/updated S3 Logging.
 	for _, sRaw := range addS3Logging {
 		opts, _ := buildCreateS3(sRaw, d.Id(), latestVersion)
+
+		// @HACK for a TF SDK Issue.
+		//
+		// This ensures that the required, `name`, field is present.
+		//
+		// If we have made it this far and `name` is not present, it is most-likely due
+		// to a defunct diff as noted here - https://github.com/hashicorp/terraform-plugin-sdk/issues/160#issuecomment-522935697.
+		//
+		// This is caused by using a StateFunc in a nested TypeSet. While the StateFunc
+		// properly handles setting state with the StateFunc, it returns extra entries
+		// during state Gets, specifically `GetChange("s3logging")` in this case.
+		if opts.Name == "" {
+			continue
+		}
+
 		err := createS3(conn, opts)
 		if err != nil {
 			return err
@@ -160,6 +175,8 @@ func (h *S3LoggingServiceAttributeHandler) Register(s *schema.Resource) error {
 					Type:        schema.TypeString,
 					Optional:    true,
 					Description: "A PGP public key that Fastly will use to encrypt your log files before writing them to disk.",
+					// Related issue for weird behavior - https://github.com/hashicorp/terraform-plugin-sdk/issues/160
+					StateFunc: trimSpaceStateFunc,
 				},
 				"response_condition": {
 					Type:        schema.TypeString,

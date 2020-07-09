8420a1510c6f4fe0b9ad62d11c3707407e2bee0d
Guy Richardson <guy@Opencredos-MacBook-Pro.local>
2020-07-09 10:38:51 +0100

    Added comment to ImportStateVerifyIgnore

--- fastly/resource_fastly_service_compute_test.go
+++ fastly/resource_fastly_service_compute_test.go
@@ -161,6 +161,7 @@ func TestAccFastlyServiceComputeV1_import(t *testing.T) {
 				ResourceName:            "fastly_service_compute.foo",
 				ImportState:             true,
 				ImportStateVerify:       true,
+				// These attributes are not stored on the Fastly API and must be ignored.
 				ImportStateVerifyIgnore: []string{"activate", "force_destroy", "package.0.filename"},
 			},
 		},
--- fastly/resource_fastly_service_v1_test.go
+++ fastly/resource_fastly_service_v1_test.go
@@ -583,6 +583,7 @@ func TestAccFastlyServiceV1_import(t *testing.T) {
 				ResourceName:            "fastly_service_v1.foo",
 				ImportState:             true,
 				ImportStateVerify:       true,
+				// These attributes are not stored on the Fastly API and must be ignored.
 				ImportStateVerifyIgnore: []string{"activate", "force_destroy"},
 			},
 		},

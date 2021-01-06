ed81f51d33a1495f015bb08153fb53429d4ac741
Daniel Malon <daniel.malon@me.com>
2021-01-06 23:07:55 +0000

    Update fastly/resource_fastly_service_compute.go
    Co-authored-by: Mark McDonnell <Integralist@users.noreply.github.com>

--- fastly/resource_fastly_service_compute.go
+++ fastly/resource_fastly_service_compute.go
@@ -42,7 +42,7 @@ var computeService = &BaseServiceDefinition{
 		NewServiceLoggingOpenstack(computeAttributes),
 		NewServiceLoggingDigitalOcean(computeAttributes),
 		NewServiceLoggingCloudfiles(computeAttributes),
-		NewServiceLoggingKinesis(vclAttributes),
+		NewServiceLoggingKinesis(computeAttributes),
 		NewServicePackage(computeAttributes),
 	},
 }

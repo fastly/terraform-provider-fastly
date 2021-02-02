bfcaf95640e212582aa859c92b8618d80d869366
Mark McDonnell <integralist@fastly.com>
2021-02-02 13:57:55 +0000

    remove print from script

--- scripts/generate-docs.go
+++ scripts/generate-docs.go
@@ -159,7 +159,6 @@ func renderPages(t *template.Template, pages []Page) {
 func renderPage(t *template.Template, p Page) {
 	basePath := filepath.Dir(p.path)
 	err := makeDirectoryIfNotExists(basePath)
-	log.Println(p.path)
 	if err != nil {
 		log.Fatal(err)
 	}

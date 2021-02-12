0efdb5cb5990773a4dfc6952654ac1496f3161bc
Mark McDonnell <integralist@fastly.com>
2021-02-12 12:35:41 +0000

    fix datadog

--- fastly/block_fastly_service_v1_logging_datadog.go
+++ fastly/block_fastly_service_v1_logging_datadog.go
@@ -36,8 +36,11 @@ func (h *DatadogServiceAttributeHandler) Process(d *schema.ResourceData, latestV
 	newSet := nd.(*schema.Set)
 
 	setDiff := NewSetDiff(func(resource interface{}) (interface{}, error) {
-		// Use the resource endpoint name as the key
-		return resource.(map[string]interface{})["name"], nil
+		t, ok := resource.(map[string]interface{})
+		if !ok {
+			return nil, fmt.Errorf("resource failed to be type asserted: %+v", resource)
+		}
+		return t["name"], nil
 	})
 
 	diffResult, err := setDiff.Diff(oldSet, newSet)
@@ -45,10 +48,10 @@ func (h *DatadogServiceAttributeHandler) Process(d *schema.ResourceData, latestV
 		return err
 	}
 
-	// DELETE old Datadog logging endpoints.
-	for _, oRaw := range diffResult.Deleted {
-		of := oRaw.(map[string]interface{})
-		opts := h.buildDelete(of, serviceID, latestVersion)
+	// DELETE removed resources
+	for _, resource := range diffResult.Deleted {
+		resource := resource.(map[string]interface{})
+		opts := h.buildDelete(resource, serviceID, latestVersion)
 
 		log.Printf("[DEBUG] Fastly Datadog logging endpoint removal opts: %#v", opts)
 
@@ -57,14 +60,62 @@ func (h *DatadogServiceAttributeHandler) Process(d *schema.ResourceData, latestV
 		}
 	}
 
-	// POST new/updated Datadog logging endpoints.
-	for _, nRaw := range diffResult.Added {
-		df := nRaw.(map[string]interface{})
-		opts := h.buildCreate(df, serviceID, latestVersion)
+	// ADD new resources
+	for _, resource := range diffResult.Added {
+		resource := resource.(map[string]interface{})
+		opts := h.buildCreate(resource, serviceID, latestVersion)
 
 		log.Printf("[DEBUG] Fastly Datadog logging addition opts: %#v", opts)
 
 		if err := createDatadog(conn, opts); err != nil {
+			return err
+		}
+	}
+
+	// UPDATE modified resources
+	//
+	// NOTE: although the go-fastly API client enables updating of a resource by
+	// its 'name' attribute, this isn't possible within terraform due to
+	// constraints in the data model/schema of the resources not having a uid.
+	for _, resource := range diffResult.Modified {
+		resource := resource.(map[string]interface{})
+
+		opts := gofastly.UpdateDatadogInput{
+			ServiceID:      d.Id(),
+			ServiceVersion: latestVersion,
+			Name:           resource["name"].(string),
+		}
+
+		// only attempt to update attributes that have changed
+		modified := setDiff.Filter(resource, oldSet)
+
+		// NOTE: where we transition between interface{} we lose the ability to
+		// infer the underlying type being either a uint vs an int. This
+		// materializes as a panic (yay) and so it's only at runtime we discover
+		// this and so we've updated the below code to convert the type asserted
+		// int into a uint before passing the value to gofastly.Uint().
+		if v, ok := modified["token"]; ok {
+			opts.Token = gofastly.String(v.(string))
+		}
+		if v, ok := modified["region"]; ok {
+			opts.Region = gofastly.String(v.(string))
+		}
+		if v, ok := modified["format"]; ok {
+			opts.Format = gofastly.String(v.(string))
+		}
+		if v, ok := modified["format_version"]; ok {
+			opts.FormatVersion = gofastly.Uint(uint(v.(int)))
+		}
+		if v, ok := modified["response_condition"]; ok {
+			opts.ResponseCondition = gofastly.String(v.(string))
+		}
+		if v, ok := modified["placement"]; ok {
+			opts.Placement = gofastly.String(v.(string))
+		}
+
+		log.Printf("[DEBUG] Update Datadog Opts: %#v", opts)
+		_, err := conn.UpdateDatadog(&opts)
+		if err != nil {
 			return err
 		}
 	}

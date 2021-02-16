3b2ed52ea92d59e6514361e19752096500413d7a
Mark McDonnell <integralist@fastly.com>
2021-02-16 14:09:18 +0000

    fix syslog

--- fastly/block_fastly_service_v1_syslog.go
+++ fastly/block_fastly_service_v1_syslog.go
@@ -34,8 +34,11 @@ func (h *SyslogServiceAttributeHandler) Process(d *schema.ResourceData, latestVe
 	newSet := ns.(*schema.Set)
 
 	setDiff := NewSetDiff(func(resource interface{}) (interface{}, error) {
-		// Use the resource endpoint name as the key
-		return resource.(map[string]interface{})["name"], nil
+		t, ok := resource.(map[string]interface{})
+		if !ok {
+			return nil, fmt.Errorf("resource failed to be type asserted: %+v", resource)
+		}
+		return t["name"], nil
 	})
 
 	diffResult, err := setDiff.Diff(oldSet, newSet)
@@ -43,13 +46,13 @@ func (h *SyslogServiceAttributeHandler) Process(d *schema.ResourceData, latestVe
 		return err
 	}
 
-	// DELETE old syslog configurations
-	for _, pRaw := range diffResult.Deleted {
-		slf := pRaw.(map[string]interface{})
+	// DELETE removed resources
+	for _, resource := range diffResult.Deleted {
+		resource := resource.(map[string]interface{})
 		opts := gofastly.DeleteSyslogInput{
 			ServiceID:      d.Id(),
 			ServiceVersion: latestVersion,
-			Name:           slf["name"].(string),
+			Name:           resource["name"].(string),
 		}
 
 		log.Printf("[DEBUG] Fastly Syslog removal opts: %#v", opts)
@@ -63,24 +66,24 @@ func (h *SyslogServiceAttributeHandler) Process(d *schema.ResourceData, latestVe
 		}
 	}
 
-	// POST new/updated Syslog
-	for _, pRaw := range diffResult.Added {
-		slf := pRaw.(map[string]interface{})
+	// ADD new resources
+	for _, resource := range diffResult.Added {
+		resource := resource.(map[string]interface{})
 
-		var vla = h.getVCLLoggingAttributes(slf)
+		var vla = h.getVCLLoggingAttributes(resource)
 		opts := gofastly.CreateSyslogInput{
 			ServiceID:         d.Id(),
 			ServiceVersion:    latestVersion,
-			Name:              slf["name"].(string),
-			Address:           slf["address"].(string),
-			Port:              uint(slf["port"].(int)),
-			Token:             slf["token"].(string),
-			UseTLS:            gofastly.Compatibool(slf["use_tls"].(bool)),
-			TLSHostname:       slf["tls_hostname"].(string),
-			TLSCACert:         slf["tls_ca_cert"].(string),
-			TLSClientCert:     slf["tls_client_cert"].(string),
-			TLSClientKey:      slf["tls_client_key"].(string),
-			MessageType:       slf["message_type"].(string),
+			Name:              resource["name"].(string),
+			Address:           resource["address"].(string),
+			Port:              uint(resource["port"].(int)),
+			Token:             resource["token"].(string),
+			UseTLS:            gofastly.Compatibool(resource["use_tls"].(bool)),
+			TLSHostname:       resource["tls_hostname"].(string),
+			TLSCACert:         resource["tls_ca_cert"].(string),
+			TLSClientCert:     resource["tls_client_cert"].(string),
+			TLSClientKey:      resource["tls_client_key"].(string),
+			MessageType:       resource["message_type"].(string),
 			Format:            vla.format,
 			FormatVersion:     uintOrDefault(vla.formatVersion),
 			ResponseCondition: vla.responseCondition,
@@ -93,6 +96,82 @@ func (h *SyslogServiceAttributeHandler) Process(d *schema.ResourceData, latestVe
 			return err
 		}
 	}
+
+	// UPDATE modified resources
+	//
+	// NOTE: although the go-fastly API client enables updating of a resource by
+	// its 'name' attribute, this isn't possible within terraform due to
+	// constraints in the data model/schema of the resources not having a uid.
+	for _, resource := range diffResult.Modified {
+		resource := resource.(map[string]interface{})
+
+		opts := gofastly.UpdateSyslogInput{
+			ServiceID:      d.Id(),
+			ServiceVersion: latestVersion,
+			Name:           resource["name"].(string),
+		}
+
+		// only attempt to update attributes that have changed
+		modified := setDiff.Filter(resource, oldSet)
+
+		// NOTE: where we transition between interface{} we lose the ability to
+		// infer the underlying type being either a uint vs an int. This
+		// materializes as a panic (yay) and so it's only at runtime we discover
+		// this and so we've updated the below code to convert the type asserted
+		// int into a uint before passing the value to gofastly.Uint().
+		if v, ok := modified["address"]; ok {
+			opts.Address = gofastly.String(v.(string))
+		}
+		if v, ok := modified["hostname"]; ok {
+			opts.Hostname = gofastly.String(v.(string))
+		}
+		if v, ok := modified["port"]; ok {
+			opts.Port = gofastly.Uint(uint(v.(int)))
+		}
+		if v, ok := modified["use_tls"]; ok {
+			opts.UseTLS = gofastly.CBool(v.(bool))
+		}
+		if v, ok := modified["ipv4"]; ok {
+			opts.IPV4 = gofastly.String(v.(string))
+		}
+		if v, ok := modified["tls_ca_cert"]; ok {
+			opts.TLSCACert = gofastly.String(v.(string))
+		}
+		if v, ok := modified["tls_hostname"]; ok {
+			opts.TLSHostname = gofastly.String(v.(string))
+		}
+		if v, ok := modified["tls_client_cert"]; ok {
+			opts.TLSClientCert = gofastly.String(v.(string))
+		}
+		if v, ok := modified["tls_client_key"]; ok {
+			opts.TLSClientKey = gofastly.String(v.(string))
+		}
+		if v, ok := modified["token"]; ok {
+			opts.Token = gofastly.String(v.(string))
+		}
+		if v, ok := modified["format"]; ok {
+			opts.Format = gofastly.String(v.(string))
+		}
+		if v, ok := modified["format_version"]; ok {
+			opts.FormatVersion = gofastly.Uint(uint(v.(int)))
+		}
+		if v, ok := modified["message_type"]; ok {
+			opts.MessageType = gofastly.String(v.(string))
+		}
+		if v, ok := modified["response_condition"]; ok {
+			opts.ResponseCondition = gofastly.String(v.(string))
+		}
+		if v, ok := modified["placement"]; ok {
+			opts.Placement = gofastly.String(v.(string))
+		}
+
+		log.Printf("[DEBUG] Update Syslog Opts: %#v", opts)
+		_, err := conn.UpdateSyslog(&opts)
+		if err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
 

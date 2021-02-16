a0b184b7118163838cbb592f2e3d54c671a90086
Mark McDonnell <integralist@fastly.com>
2021-02-16 18:20:55 +0000

    fix response object

--- fastly/block_fastly_service_v1_responseobject.go
+++ fastly/block_fastly_service_v1_responseobject.go
@@ -34,8 +34,11 @@ func (h *ResponseObjectServiceAttributeHandler) Process(d *schema.ResourceData, 
 	newSet := nr.(*schema.Set)
 
 	setDiff := NewSetDiff(func(resource interface{}) (interface{}, error) {
-		// Use the resource name as the key
-		return resource.(map[string]interface{})["name"], nil
+		t, ok := resource.(map[string]interface{})
+		if !ok {
+			return nil, fmt.Errorf("resource failed to be type asserted: %+v", resource)
+		}
+		return t["name"], nil
 	})
 
 	diffResult, err := setDiff.Diff(oldSet, newSet)
@@ -43,13 +46,13 @@ func (h *ResponseObjectServiceAttributeHandler) Process(d *schema.ResourceData, 
 		return err
 	}
 
-	// DELETE old response object configurations
-	for _, rRaw := range diffResult.Deleted {
-		rf := rRaw.(map[string]interface{})
+	// DELETE removed resources
+	for _, resource := range diffResult.Deleted {
+		resource := resource.(map[string]interface{})
 		opts := gofastly.DeleteResponseObjectInput{
 			ServiceID:      d.Id(),
 			ServiceVersion: latestVersion,
-			Name:           rf["name"].(string),
+			Name:           resource["name"].(string),
 		}
 
 		log.Printf("[DEBUG] Fastly Response Object removal opts: %#v", opts)
@@ -63,20 +66,20 @@ func (h *ResponseObjectServiceAttributeHandler) Process(d *schema.ResourceData, 
 		}
 	}
 
-	// POST new/updated Response Object
-	for _, rRaw := range diffResult.Added {
-		rf := rRaw.(map[string]interface{})
+	// ADD new resources
+	for _, resource := range diffResult.Added {
+		resource := resource.(map[string]interface{})
 
 		opts := gofastly.CreateResponseObjectInput{
 			ServiceID:        d.Id(),
 			ServiceVersion:   latestVersion,
-			Name:             rf["name"].(string),
-			Status:           uint(rf["status"].(int)),
-			Response:         rf["response"].(string),
-			Content:          rf["content"].(string),
-			ContentType:      rf["content_type"].(string),
-			RequestCondition: rf["request_condition"].(string),
-			CacheCondition:   rf["cache_condition"].(string),
+			Name:             resource["name"].(string),
+			Status:           uint(resource["status"].(int)),
+			Response:         resource["response"].(string),
+			Content:          resource["content"].(string),
+			ContentType:      resource["content_type"].(string),
+			RequestCondition: resource["request_condition"].(string),
+			CacheCondition:   resource["cache_condition"].(string),
 		}
 
 		log.Printf("[DEBUG] Create Response Object Opts: %#v", opts)
@@ -85,6 +88,55 @@ func (h *ResponseObjectServiceAttributeHandler) Process(d *schema.ResourceData, 
 			return err
 		}
 	}
+
+	// UPDATE modified resources
+	//
+	// NOTE: although the go-fastly API client enables updating of a resource by
+	// its 'name' attribute, this isn't possible within terraform due to
+	// constraints in the data model/schema of the resources not having a uid.
+	for _, resource := range diffResult.Modified {
+		resource := resource.(map[string]interface{})
+
+		opts := gofastly.UpdateResponseObjectInput{
+			ServiceID:      d.Id(),
+			ServiceVersion: latestVersion,
+			Name:           resource["name"].(string),
+		}
+
+		// only attempt to update attributes that have changed
+		modified := setDiff.Filter(resource, oldSet)
+
+		// NOTE: where we transition between interface{} we lose the ability to
+		// infer the underlying type being either a uint vs an int. This
+		// materializes as a panic (yay) and so it's only at runtime we discover
+		// this and so we've updated the below code to convert the type asserted
+		// int into a uint before passing the value to gofastly.Uint().
+		if v, ok := modified["status"]; ok {
+			opts.Status = gofastly.Uint(uint(v.(int)))
+		}
+		if v, ok := modified["response"]; ok {
+			opts.Response = gofastly.String(v.(string))
+		}
+		if v, ok := modified["content"]; ok {
+			opts.Content = gofastly.String(v.(string))
+		}
+		if v, ok := modified["content_type"]; ok {
+			opts.ContentType = gofastly.String(v.(string))
+		}
+		if v, ok := modified["request_condition"]; ok {
+			opts.RequestCondition = gofastly.String(v.(string))
+		}
+		if v, ok := modified["cache_condition"]; ok {
+			opts.CacheCondition = gofastly.String(v.(string))
+		}
+
+		log.Printf("[DEBUG] Update Response Object Opts: %#v", opts)
+		_, err := conn.UpdateResponseObject(&opts)
+		if err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
 

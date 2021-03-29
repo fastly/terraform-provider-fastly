c6fc600e582989e824737dc27e73439a61b4e5fd
Ben Gesoff <ben.gesoff@opencredo.com>
2021-03-29 12:43:50 +0100

    Add debug mode (#386)
    * Add support for running plugin in debug mode
    
    * Add debug instructions to the README

--- main.go
+++ main.go
@@ -1,11 +1,30 @@
 package main
 
 import (
-	"github.com/fastly/terraform-provider-fastly/fastly"
+	"context"
+	"flag"
+	"log"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+
+	"github.com/fastly/terraform-provider-fastly/fastly"
 )
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: fastly.Provider})
+	var debugMode bool
+
+	flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	opts := &plugin.ServeOpts{ProviderFunc: fastly.Provider}
+
+	if debugMode {
+		err := plugin.Debug(context.Background(), "fastly/fastly", opts)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		return
+	}
+
+	plugin.Serve(opts)
 }

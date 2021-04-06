edde46cf7767cc98727f33f50866375fca892f5e
Ben Gesoff <ben.gesoff@opencredo.com>
2021-04-06 11:11:31 +0100

    PATCH endpoint for TLS subscriptions (#370)
    * Add update function to TLS Subscriptions resource
    
    Unable to test as it requires a subscription to be in the "issued" state
    which would depend on another provider to handle DNS.
    
    Takes advantage of Terraform's CustomizeDiff to decide whether to update
    the resource in place or recreate it.
    
    * Test updating subscription
    
    Unfortunately can't test the PATCH endpoint as it requires the subscription being in "issued" state. However, we can at least test that the resource correctly decides not to use the PATCH endpoint as a result and instead recreates the resource.
    
    * Add force_update argument to opt-in to potentially dangerous operation
    
    Same logic as the force_destroy argument.
    
    * Rename test variables for clarity

--- fastly/resource_fastly_tls_subscription_test.go
+++ fastly/resource_fastly_tls_subscription_test.go
@@ -21,7 +21,10 @@ func init() {
 
 func TestAccResourceFastlyTLSSubscription(t *testing.T) {
 	name := acctest.RandomWithPrefix(testResourcePrefix)
-	domain := fmt.Sprintf("%s.test", name)
+	domain1 := fmt.Sprintf("%s.test", name)
+	domain2 := fmt.Sprintf("%sALT.test", name)
+	commonName1 := domain1
+	commonName2 := domain2
 
 	resourceName := "fastly_tls_subscription.subject"
 	resource.ParallelTest(t, resource.TestCase{
@@ -30,7 +33,7 @@ func TestAccResourceFastlyTLSSubscription(t *testing.T) {
 		CheckDestroy:      testAccCheckServiceV1Destroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccResourceFastlyTLSSubscriptionConfig(name, domain),
+				Config: testAccResourceFastlyTLSSubscriptionConfig(name, domain1, domain2, commonName1),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttrSet(resourceName, "configuration_id"),
 					resource.TestCheckResourceAttrSet(resourceName, "created_at"),
@@ -38,15 +41,19 @@ func TestAccResourceFastlyTLSSubscription(t *testing.T) {
 					resource.TestCheckResourceAttrSet(resourceName, "state"),
 					resource.TestCheckResourceAttr(resourceName, "managed_dns_challenge.%", "3"),
 					resource.TestCheckResourceAttrSet(resourceName, "managed_http_challenges.#"),
-					resource.TestCheckResourceAttr(resourceName, "common_name", domain),
+					resource.TestCheckResourceAttr(resourceName, "common_name", domain1),
 					testAccResourceFastlyTLSSubscriptionExists(resourceName),
 				),
+			},
+			{
+				Config: testAccResourceFastlyTLSSubscriptionConfig(name, domain1, domain2, commonName2),
+				Check:  resource.TestCheckResourceAttr(resourceName, "common_name", domain2),
 			},
 			{
 				ResourceName:            resourceName,
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"force_destroy"},
+				ImportStateVerifyIgnore: []string{"force_destroy", "force_update"},
 			},
 			{
 				Config:      testAccResourceFastlyTLSSubscriptionConfig_invalidCommonName(),
@@ -56,10 +63,14 @@ func TestAccResourceFastlyTLSSubscription(t *testing.T) {
 	})
 }
 
-func testAccResourceFastlyTLSSubscriptionConfig(name, domain string) string {
+func testAccResourceFastlyTLSSubscriptionConfig(name, domain1, domain2, commonName string) string {
 	return fmt.Sprintf(`
 resource "fastly_service_v1" "test" {
   name = "%s"
+
+  domain {
+    name = "%s"
+  }
 
   domain {
     name = "%s"
@@ -74,9 +85,10 @@ resource "fastly_service_v1" "test" {
 }
 resource "fastly_tls_subscription" "subject" {
   domains = [for domain in fastly_service_v1.test.domain : domain.name]
+  common_name = "%s"
   certificate_authority = "lets-encrypt"
 }
-`, name, domain)
+`, name, domain1, domain2, commonName)
 }
 
 func testAccResourceFastlyTLSSubscriptionExists(resourceName string) resource.TestCheckFunc {

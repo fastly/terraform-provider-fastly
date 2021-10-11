b6362a1ce71ce59e1604f875d08c8821dc46d33f
Shohei Maeda <irt_m.jrsyo@ntworkers.com>
2021-10-11 23:34:44 +0900

    Add new `force_http2` provider option

--- fastly/provider.go
+++ fastly/provider.go
@@ -33,6 +33,12 @@ func Provider() *schema.Provider {
 				Default:     false,
 				Description: "Set this to `true` if you only need data source that does not require authentication such as `fastly_ip_ranges`",
 			},
+			"force_http2": {
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Default:     false,
+				Description: "Set this to `true` to disable HTTP/1.x fallback mechanism that the underlying Go library will attempt upon connection to `api.fastly.com:443` by default. This may slightly improve the provider's performance and reduce unnecessary TLS handshakes. Default: `false`",
+			},
 		},
 		DataSourcesMap: map[string]*schema.Resource{
 			"fastly_ip_ranges":                    dataSourceFastlyIPRanges(),
@@ -70,10 +76,11 @@ func Provider() *schema.Provider {
 
 	provider.ConfigureContextFunc = func(_ context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
 		config := Config{
-			ApiKey:    d.Get("api_key").(string),
-			BaseURL:   d.Get("base_url").(string),
-			NoAuth:    d.Get("no_auth").(bool),
-			UserAgent: provider.UserAgent(TerraformProviderProductUserAgent, version.ProviderVersion),
+			ApiKey:     d.Get("api_key").(string),
+			BaseURL:    d.Get("base_url").(string),
+			NoAuth:     d.Get("no_auth").(bool),
+			ForceHttp2: d.Get("force_http2").(bool),
+			UserAgent:  provider.UserAgent(TerraformProviderProductUserAgent, version.ProviderVersion),
 		}
 		return config.Client()
 	}

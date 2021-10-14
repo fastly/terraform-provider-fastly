ea711bce7441ef8952d2be4221a23f9d57125d85
Shohei Maeda <irt_m.jrsyo@ntworkers.com>
2021-10-14 18:22:27 +0900

    tweak test code

--- fastly/config_test.go
+++ fastly/config_test.go
@@ -32,12 +32,12 @@ func TestForceHttp2(t *testing.T) {
 	client2, _ := c2.Client()
 
 	tv1 := reflect.ValueOf(client1.conn.HTTPClient.Transport).Elem()
-	// <http.Transport Value>
-	ts1 := tv1.FieldByName("transport").Elem().Elem().String()
+	// http.Transport
+	ts1 := reflect.Indirect(tv1.FieldByName("transport").Elem()).Type().String()
 
 	tv2 := reflect.ValueOf(client2.conn.HTTPClient.Transport).Elem()
-	// <http2.Transport Value>
-	ts2 := tv2.FieldByName("transport").Elem().Elem().String()
+	// http2.Transport
+	ts2 := reflect.Indirect(tv2.FieldByName("transport").Elem()).Type().String()
 
 	if ts1 == ts2 {
 		t.Errorf("Failed to create client with force_http2: %#v, %#v", ts1, ts2)

36b1fc29e4d36eae8cf8351a8402bcfcf29c4e91
Shohei Maeda <irt_m.jrsyo@ntworkers.com>
2021-10-14 18:29:21 +0900

    Override `Transport` to enable keepalive and add new `force_http2` provider option (#485)
    * Use `http2.Transport` to reduce TLS handshake upon initial client requests
    
    * Add new `force_http2` provider option
    
    * update code comment
    
    Co-authored-by: Mark McDonnell <Integralist@users.noreply.github.com>
    
    * add test
    
    * tweak test code
    
    Co-authored-by: Mark McDonnell <Integralist@users.noreply.github.com>

--- fastly/config.go
+++ fastly/config.go
@@ -2,17 +2,20 @@ package fastly
 
 import (
 	"fmt"
+	"net/http"
 
 	gofastly "github.com/fastly/go-fastly/v5/fastly"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/logging"
+	"golang.org/x/net/http2"
 )
 
 type Config struct {
-	ApiKey    string
-	BaseURL   string
-	UserAgent string
-	NoAuth    bool
+	ApiKey     string
+	BaseURL    string
+	UserAgent  string
+	NoAuth     bool
+	ForceHttp2 bool
 }
 
 type FastlyClient struct {
@@ -31,6 +34,25 @@ func (c *Config) Client() (*FastlyClient, diag.Diagnostics) {
 	fastlyClient, err := gofastly.NewClientForEndpoint(c.ApiKey, c.BaseURL)
 	if err != nil {
 		return nil, diag.FromErr(err)
+	}
+
+	// NOTE: We're fixing two issues here.
+	// 1 (critical). go-fastly uses cleanhttp module that would disable keepalive connection:
+	// https://github.com/hashicorp/go-cleanhttp/blob/v0.5.2/cleanhttp.go#L14-L15
+	// this consumes local ports (source ports) more than necessary that could impact
+	// some of the clients under restricted NAT environments such as:
+	// https://github.com/fastly/terraform-provider-fastly/issues/484
+	// overriding it with the default (still non-shared transport) so we can enable keepalive
+	//
+	// 2 (minor). while http.Transport supports HTTP/2 by default, it does TLS-ALPN negotiation
+	// in order to support HTTP/1.x fallback. This means each new client connection initiated
+	// by each resource will start TLS handshake regardless of the existing connection pool status.
+	// explicitly assigning http2.Transport so there will be just one TLS-ALPN negotiation happening
+	// (across all Fastly provider resources) against the same api.fastly.com:443 destination.
+	if c.ForceHttp2 {
+		fastlyClient.HTTPClient.Transport = &http2.Transport{}
+	} else {
+		fastlyClient.HTTPClient.Transport = &http.Transport{}
 	}
 
 	fastlyClient.HTTPClient.Transport = logging.NewTransport("Fastly", fastlyClient.HTTPClient.Transport)

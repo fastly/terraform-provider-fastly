5c85976639cb54aff293311cbb5a21aff0117805
Integralist <integralist@fastly.com>
2022-09-23 17:07:31 +0100

    further changes

--- fastly/block_fastly_service_package.go
+++ fastly/block_fastly_service_package.go
@@ -55,9 +55,8 @@ func (h *PackageServiceAttributeHandler) Register(s *schema.Resource) error {
 				},
 				"source_code_hash": {
 					Type:        schema.TypeString,
-					Optional:    true,
 					Computed:    true,
-					Description: `Used to trigger updates. Must be set to a SHA512 hash of the package file specified with the filename. The usual way to set this is filesha512("package.tar.gz") (Terraform 0.11.12 and later) or filesha512(file("package.tar.gz")) (Terraform 0.11.11 and earlier), where "package.tar.gz" is the local filename of the Wasm deployment package`,
+					Description: `Used to trigger updates. Is automatically set to a SHA512 hash of the package file.`,
 				},
 			},
 		},
@@ -67,6 +66,8 @@ func (h *PackageServiceAttributeHandler) Register(s *schema.Resource) error {
 
 // Process creates or updates the attribute against the Fastly API.
 func (h *PackageServiceAttributeHandler) Process(_ context.Context, d *schema.ResourceData, latestVersion int, conn *gofastly.Client) error {
+	id := d.Id()
+	log.Printf("[DEBUG] Processing package for Service ID %s", id)
 	if v, ok := d.GetOk(h.GetKey()); ok {
 		// Schema guarantees one package block.
 		pkg := v.([]interface{})[0].(map[string]interface{})
@@ -78,57 +79,53 @@ func (h *PackageServiceAttributeHandler) Process(_ context.Context, d *schema.Re
 		packageURL := pkg["url"].(string)
 		packageFilename := pkg["filename"].(string)
 
+		// We don't overwrite packageFilename as we need to ensure the original
+		// value can be persisted back to the state.
+		packagePath := packageFilename
+
 		if packageURL != "" {
-			f, err := os.CreateTemp("", "package-*.tar.gz")
+			f, err := downloadPackage(packageURL)
 			if err != nil {
-				return fmt.Errorf("unable to create a temporary file to copy package data into: %w", err)
-			}
-			log.Printf("[DEBUG] Temp Package file %s", f.Name())
-			defer os.Remove(f.Name())
-
-			resp, err := http.Get(packageURL)
-			if err != nil {
-				return fmt.Errorf("unable to download package '%s': %w", packageURL, err)
-			}
-			defer resp.Body.Close()
-
-			if resp.StatusCode != http.StatusOK {
-				return fmt.Errorf("bad package response '%s': %s", packageURL, resp.Status)
-			}
-			log.Printf("[DEBUG] Downloaded Package file from %s", packageURL)
-
-			_, err = io.Copy(f, resp.Body)
-			if err != nil {
-				return fmt.Errorf("unable copy package into temporary file: %w", err)
+				return err
 			}
 
-			digest, err := fileSHA512(f.Name())
-			if err != nil {
-				return fmt.Errorf("unable to hash package content: %w", err)
-			}
-			log.Printf("[DEBUG] Package hash digest %s", digest)
-			sch := d.Get("source_code_hash")
-			if sch != nil {
-				v := sch.(string)
-				fmt.Printf("current source_code_hash: %+v\n", v)
-			}
+			filename := f.Name()
+			defer os.Remove(filename)
 
-			wp := flattenPackage(digest, packageFilename, packageURL)
-			key := h.GetKey()
-			if err := d.Set(key, wp); err != nil {
-				log.Printf("[WARN] Error setting Package for (%s): %s", d.Id(), err)
-			}
+			packagePath = filename
+		}
 
-			packageFilename = f.Name()
+		// NOTE: We can't prevent a package upload, even if the hash hasn't changed.
+		// This is because we can't tell if HasChange() returned false because it
+		// was the result of there being no change to the package's hashsum OR if
+		// it was because this was the first time Process() was called, such as when
+		// starting a new project.
+		if d.HasChange("source_code_hash") {
+			log.Print("[DEBUG] Package hash digest changed")
 		}
 
 		err := updatePackage(conn, &gofastly.UpdatePackageInput{
-			ServiceID:      d.Id(),
+			ServiceID:      id,
 			ServiceVersion: latestVersion,
-			PackagePath:    packageFilename,
+			PackagePath:    packagePath,
 		})
 		if err != nil {
-			return fmt.Errorf("error modifying package %s: %s", d.Id(), err)
+			return fmt.Errorf("error modifying package %s: %s", id, err)
+		}
+
+		// Once we've safely uploaded the package, we can then ensure the state file
+		// is updated with the latest package hashsum.
+
+		digest, err := fileSHA512(packagePath)
+		if err != nil {
+			return fmt.Errorf("unable to hash package content: %w", err)
+		}
+		log.Printf("[DEBUG] Package hash digest %s", digest)
+
+		wp := flattenPackage(digest, packageFilename, packageURL)
+		key := h.GetKey()
+		if err := d.Set(key, wp); err != nil {
+			log.Printf("[WARN] Error setting Package for (%s): %s", id, err)
 		}
 	}
 
@@ -152,12 +149,64 @@ func (h *PackageServiceAttributeHandler) Read(_ context.Context, d *schema.Resou
 		return fmt.Errorf("error looking up Package for (%s), version (%v): %v", id, s.ActiveVersion.Number, err)
 	}
 
+	// The package schema dictates only one of `url` or `filename` can be set.
 	// NOTE: d.Get() is pulling the data from the state file.
 	// TODO: Interpolate h.GetKey() to avoid error if package resource changes.
 	packageURL := d.Get("package.0.url").(string)
-	filename := d.Get("package.0.filename").(string)
-	wp := flattenPackage(pkg.Metadata.HashSum, filename, packageURL)
+	packageFilename := d.Get("package.0.filename").(string)
+
+	// This hash represents the 'active' package's hash (coming from the API)
+	hashSum := pkg.Metadata.HashSum
+
+	// We don't overwrite packageFilename as we need to ensure the original
+	// value can be persisted back to the state.
+	filename := packageFilename
+
+	if packageURL != "" {
+		// TODO: Check if hashes differ, only then let file persist to disk.
+		// If the hashes don't differ, then Process() won't be called. If we allow
+		// the file to persist to disk, because we know Process() will be called,
+		// then we can have Process() handle the file cleanup. This will avoid the
+		// current situation, which is both Read() and Process() downloading the
+		// package.
+		//
+		// 🚨 Read() doesn't always get called before Process(). If this is the
+		// first time that `terraform plan` is called (e.g. a new project) then
+		// there is no state, and so Terraform will go straight to `Process()`.
+		f, err := downloadPackage(packageURL)
+		if err != nil {
+			return err
+		}
+
+		filename = f.Name()
+		defer os.Remove(filename)
+	}
+
+	localDigest, err := fileSHA512(filename)
+	if err != nil {
+		return fmt.Errorf("unable to hash package content: %w", err)
+	}
+	log.Printf("[DEBUG] Package hash digest %s", localDigest)
+
+	// If the specified package file has a different hash to what the API
+	// returns (e.g. the active package is not the same code) then we'll update
+	// the state to use the new hash, otherwise the hash from the API is used.
+	//
+	// The potential change in the state's hash value will cause Terraform to
+	// conclude there is a difference and will need to execute Process().
+	if localDigest != hashSum {
+		hashSum = localDigest
+		log.Print("[DEBUG] Package hash does not match the API returned package hash")
+	} else {
+		log.Print("[DEBUG] Package hash matches the API returned package hash")
+	}
+
+	// Update the state file before comparing to the Terraform configuration file...
+
+	wp := flattenPackage(hashSum, packageFilename, packageURL)
 	key := h.GetKey()
+
+	log.Printf("[DEBUG] Setting state for key '%s'", key)
 	if err := d.Set(key, wp); err != nil {
 		log.Printf("[WARN] Error setting Package for (%s): %s", id, err)
 	}
@@ -181,6 +230,35 @@ func flattenPackage(hashSum, filename, packageURL string) []map[string]interface
 	// Convert Package to a map for saving to state.
 	pa = append(pa, p)
 	return pa
+}
+
+// downloadPackage downloads a package from the endpoint defined in the "url"
+// attribute of the user's Terraform configuration, and returns the file
+// descriptor for the downloaded package.
+func downloadPackage(packageURL string) (f *os.File, err error) {
+	f, err = os.CreateTemp("", "package-*.tar.gz")
+	if err != nil {
+		return f, fmt.Errorf("unable to create a temporary file to copy package data into: %w", err)
+	}
+	log.Printf("[DEBUG] Temp Package file %s", f.Name())
+
+	resp, err := http.Get(packageURL)
+	if err != nil {
+		return f, fmt.Errorf("unable to download package '%s': %w", packageURL, err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return f, fmt.Errorf("bad package response '%s': %s", packageURL, resp.Status)
+	}
+	log.Printf("[DEBUG] Downloaded Package file from %s", packageURL)
+
+	_, err = io.Copy(f, resp.Body)
+	if err != nil {
+		return f, fmt.Errorf("unable copy package into temporary file: %w", err)
+	}
+
+	return f, err
 }
 
 // fileSHA512 reads the path content, hashes it with sha512 and hex encodes.

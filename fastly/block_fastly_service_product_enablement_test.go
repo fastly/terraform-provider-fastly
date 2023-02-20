ce80b48e79cfe28a8649a8b1356e5ae49e68ef81
Integralist <integralist@fastly.com>
2023-02-20 16:14:53 +0000

    test(product_enablement): add tests for product enablement feature

---
+++ fastly/block_fastly_service_product_enablement_test.go
@@ -0,0 +1,85 @@
+package fastly
+
+import (
+	"fmt"
+	"testing"
+
+	gofastly "github.com/fastly/go-fastly/v7/fastly"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccFastlyServiceVCLProductEnablement_basic(t *testing.T) {
+	var service gofastly.ServiceDetail
+	serviceName := fmt.Sprintf("tf-test-%s", acctest.RandString(10))
+	domainName := fmt.Sprintf("fastly-test.tf-%s.com", acctest.RandString(10))
+	backendName := fmt.Sprintf("backend-tf-%s", acctest.RandString(10))
+	backendAddress := "httpbin.org"
+
+	config := fmt.Sprintf(`
+  resource "fastly_service_vcl" "foo" {
+    name = "%s"
+
+    domain {
+      name    = "%s"
+      comment = "demo"
+    }
+
+    backend {
+      address = "%s"
+      name    = "%s"
+      port    = 443
+      shield  = "amsterdam-nl"
+    }
+
+    product_enablement {
+      brotli_compression = true
+      domain_inspector   = true
+      image_optimizer    = true
+      origin_inspector   = true
+      websockets         = true
+    }
+
+    force_destroy = true
+  }
+  `, serviceName, domainName, backendAddress, backendName)
+
+	// The following backends are what we expect to exist after all our Terraform
+	// configuration settings have been applied. We expect them to correlate to
+	// the specific backend definitions in the Terraform configuration.
+
+	b1 := gofastly.Backend{
+		Address: backendAddress,
+		Name:    backendName,
+		Port:    443,
+		Shield:  "amsterdam-nl", // required for image_optimizer
+
+		// NOTE: The following are defaults applied by the API.
+		BetweenBytesTimeout: 10000,
+		ConnectTimeout:      1000,
+		FirstByteTimeout:    15000,
+		Hostname:            backendAddress,
+		MaxConn:             200,
+		SSLCheckCert:        true,
+		Weight:              100,
+	}
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		ProviderFactories: testAccProviders,
+		CheckDestroy:      testAccCheckServiceVCLDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckServiceVCLExists("fastly_service_vcl.foo", &service),
+					resource.TestCheckResourceAttr("fastly_service_vcl.foo", "name", serviceName),
+					resource.TestCheckResourceAttr("fastly_service_vcl.foo", "backend.#", "1"),
+					testAccCheckFastlyServiceVCLBackendAttributes(&service, []*gofastly.Backend{&b1}),
+				),
+			},
+		},
+	})
+}

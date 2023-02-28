da8fe665f9a2b8346db3718085bb6d4bed63dd76
Mark McDonnell <Integralist@users.noreply.github.com>
2023-02-28 17:59:31 +0000

    Update fastly/resource_fastly_tls_subscription.go

--- fastly/resource_fastly_tls_subscription.go
+++ fastly/resource_fastly_tls_subscription.go
@@ -36,7 +36,7 @@ func resourceFastlyTLSSubscription() *schema.Resource {
 		Schema: map[string]*schema.Schema{
 			"certificate_authority": {
 				Type:         schema.TypeString,
-				Description:  "The entity that issues and certifies the TLS certificates for your subscription. Valid values are `lets-encrypt`, `globalsign` or `certainly`.",
+				Description:  "The entity that issues and certifies the TLS certificates for your subscription. Valid values are `lets-encrypt`, `globalsign` or `certainly` (beta).",
 				Required:     true,
 				ForceNew:     true,
 				ValidateFunc: validation.StringInSlice([]string{"lets-encrypt", "globalsign", "certainly"}, false),

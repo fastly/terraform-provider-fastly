4bd8c4f5193b2e7ff9de7a116d1b5f7e5e58eb01
Integralist <integralist@fastly.com>
2023-08-02 17:53:17 +0100

    feat: create fastly_kvstores data source

---
+++ fastly/data_source_kvstores.go
@@ -0,0 +1,98 @@
+package fastly
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"strconv"
+
+	gofastly "github.com/fastly/go-fastly/v8/fastly"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	"github.com/fastly/terraform-provider-fastly/fastly/hashcode"
+)
+
+func dataSourceFastlyKVStores() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceFastlyKVStoresRead,
+		Schema: map[string]*schema.Schema{
+			"stores": {
+				Type:        schema.TypeSet,
+				Computed:    true,
+				Description: "List of all KV Stores.",
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"id": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "Alphanumeric string identifying the KV Store.",
+						},
+						"name": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "Name for the KV Store.",
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func dataSourceFastlyKVStoresRead(_ context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
+	conn := meta.(*APIClient).conn
+
+	log.Printf("[DEBUG] Reading KV Stores")
+
+	var (
+		cursor string
+		stores []gofastly.KVStore
+	)
+
+	for {
+		remoteState, err := conn.ListKVStores(&gofastly.ListKVStoresInput{
+			Cursor: cursor,
+		})
+		if err != nil {
+			return diag.Errorf("error fetching KV Stores: %s", err)
+		}
+
+		if remoteState != nil {
+			stores = append(stores, remoteState.Data...)
+			c, ok := remoteState.Meta["next_cursor"]
+			if !ok || c == "" || c == cursor {
+				break
+			}
+			cursor = c
+		}
+	}
+
+	hashBase, _ := json.Marshal(stores)
+	hashString := strconv.Itoa(hashcode.String(string(hashBase)))
+	d.SetId(hashString)
+
+	if err := d.Set("stores", flattenDataSourceKVStores(stores)); err != nil {
+		return diag.Errorf("error setting stores: %s", err)
+	}
+
+	return nil
+}
+
+// flattenDataSourceKVStores models data into format suitable for saving to
+// Terraform state.
+func flattenDataSourceKVStores(remoteState []gofastly.KVStore) []map[string]any {
+	result := make([]map[string]any, len(remoteState))
+	if len(remoteState) == 0 {
+		return result
+	}
+
+	for i, resource := range remoteState {
+		result[i] = map[string]any{
+			"id":   resource.ID,
+			"name": resource.Name,
+		}
+	}
+
+	return result
+}
---
+++ fastly/data_source_kvstores_test.go
@@ -0,0 +1,86 @@
+package fastly
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+)
+
+func TestAccFastlyDataSourceKVStores_Config(t *testing.T) {
+	h := generateHex()
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		ProviderFactories: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccFastlyDataSourceKVStoresConfig(h),
+				Check: resource.ComposeTestCheckFunc(
+					func(s *terraform.State) error {
+						r := s.RootModule().Resources["data.fastly_kvstores.example"]
+						a := r.Primary.Attributes
+
+						want := generateNames(h, 3)
+						var (
+							found int
+							got   []string
+						)
+
+						// NOTE: API doesn't guarantee KV Store order.
+						for k, v := range a {
+							// Example of keys we're looking for:
+							// "stores.0.name":"tf_677f63804c9351ac31fd0cb1db697b95_1",
+							// "stores.1.name":"tf_677f63804c9351ac31fd0cb1db697b95_2",
+							// "stores.2.name":"tf_677f63804c9351ac31fd0cb1db697b95_3",
+							if strings.HasSuffix(k, ".name") {
+								got = append(got, v)
+								for _, name := range want {
+									if v == name {
+										found++
+										break
+									}
+								}
+							}
+						}
+
+						if found != len(want) {
+							return fmt.Errorf("want: %v, got: %v", want, got)
+						}
+
+						return nil
+					},
+				),
+			},
+		},
+	})
+}
+
+func testAccFastlyDataSourceKVStoresConfig(h string) string {
+	tf := `
+resource "fastly_kvstore" "example_1" {
+  name          = "tf_%s_1"
+  force_destroy = true
+}
+
+resource "fastly_kvstore" "example_2" {
+  name          = "tf_%s_2"
+  force_destroy = true
+}
+
+resource "fastly_kvstore" "example_3" {
+  name          = "tf_%s_3"
+  force_destroy = true
+}
+
+data "fastly_kvstores" "example" {
+  depends_on = [fastly_kvstore.example_1, fastly_kvstore.example_2, fastly_kvstore.example_3]
+}
+`
+
+	return fmt.Sprintf(tf, h, h, h)
+}

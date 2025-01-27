9efa280078efe6ec1464420b0386f7dc3f4706cb
Integralist <mmcdonnell@fastly.com>
2025-01-27 13:59:04 +0000

    feat(domain-v1): integrate domain-v1 functionality

---
+++ fastly/resource_fastly_domain_v1.go
@@ -0,0 +1,122 @@
+package fastly
+
+import (
+	"context"
+	"log"
+
+	gofastly "github.com/fastly/go-fastly/v9/fastly"
+	v1 "github.com/fastly/go-fastly/v9/fastly/domains/v1"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func resourceFastlyDomainV1() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceFastlyDomainV1Create,
+		ReadContext:   resourceFastlyDomainV1Read,
+		UpdateContext: resourceFastlyDomainV1Update,
+		DeleteContext: resourceFastlyDomainV1Delete,
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"domain_id": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The Domain Identifier (UUID).",
+			},
+			"fqdn": {
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: "The fully-qualified domain name for your domain. Can be created, but not updated.",
+			},
+			"service_id": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "The service_id associated with your domain or null if there is no association.",
+			},
+		},
+	}
+}
+
+func resourceFastlyDomainV1Create(_ context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
+	conn := meta.(*APIClient).conn
+
+	var input v1.CreateInput
+	if v, ok := d.GetOk("fqdn"); ok {
+		input.FQDN = gofastly.ToPointer(v.(string))
+	}
+	if v, ok := d.GetOk("service_id"); ok {
+		input.ServiceID = gofastly.ToPointer(v.(string))
+	}
+
+	data, err := v1.Create(conn, &input)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId(data.DomainID)
+
+	if err := d.Set("domain_id", data.DomainID); err != nil {
+		return diag.FromErr(err)
+	}
+	return nil
+}
+
+func resourceFastlyDomainV1Read(_ context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
+	log.Printf("[DEBUG] Refreshing Domain V1 Configuration for (%s)", d.Id())
+	conn := meta.(*APIClient).conn
+
+	input := &v1.GetInput{
+		DomainID: gofastly.ToPointer(d.Id()),
+	}
+
+	data, err := v1.Get(conn, input)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	if err := d.Set("domain_id", data.DomainID); err != nil {
+		return diag.FromErr(err)
+	}
+	if err := d.Set("fqdn", data.FQDN); err != nil {
+		return diag.FromErr(err)
+	}
+	if err := d.Set("service_id", data.ServiceID); err != nil {
+		return diag.FromErr(err)
+	}
+
+	return nil
+}
+
+func resourceFastlyDomainV1Update(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
+	conn := meta.(*APIClient).conn
+
+	input := &v1.UpdateInput{
+		DomainID: gofastly.ToPointer(d.Id()),
+	}
+	if v, ok := d.GetOk("service_id"); ok {
+		input.ServiceID = gofastly.ToPointer(v.(string))
+	}
+
+	_, err := v1.Update(conn, input)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	return resourceFastlyDomainV1Read(ctx, d, meta)
+}
+
+func resourceFastlyDomainV1Delete(_ context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
+	conn := meta.(*APIClient).conn
+	input := &v1.DeleteInput{
+		DomainID: gofastly.ToPointer(d.Id()),
+	}
+	err := v1.Delete(conn, input)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	return nil
+}
---
+++ fastly/resource_fastly_domain_v1_test.go
@@ -0,0 +1,87 @@
+package fastly
+
+import (
+	"fmt"
+	"testing"
+
+	gofastly "github.com/fastly/go-fastly/v9/fastly"
+	v1 "github.com/fastly/go-fastly/v9/fastly/domains/v1"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+)
+
+func TestAccFastlyDomainV1_Basic(t *testing.T) {
+	domainName := fmt.Sprintf("fastly-test.tf-%s.com", acctest.RandString(10))
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		ProviderFactories: testAccProviders,
+		CheckDestroy:      testAccCheckDomainV1Destroy,
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+				resource "fastly_domain_v1" "example" {
+				    fqdn = "%s"
+				}
+				`, domainName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("fastly_domain_v1.example", "fqdn", domainName),
+					resource.TestCheckNoResourceAttr("fastly_domain_v1.example", "service_id"),
+				),
+			},
+			{
+				Config: fmt.Sprintf(`
+				resource "fastly_service_vcl" "example" {
+					name = "%s"
+					domain {
+					    name = "%s"
+					}
+					force_destroy = true
+				}
+				resource "fastly_domain_v1" "example" {
+				    fqdn = "%s"
+					service_id = resource.fastly_service_vcl.example.id
+				}
+				`, domainName, domainName, domainName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("fastly_domain_v1.example", "fqdn", domainName),
+					resource.TestCheckResourceAttrSet("fastly_domain_v1.example", "service_id"),
+				),
+			},
+			{
+				ResourceName:      "fastly_domain_v1.example",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccCheckDomainV1Destroy(s *terraform.State) error {
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "fastly_domain_v1" {
+			continue
+		}
+		a := rs.Primary.Attributes
+		fqdn := a["fqdn"]
+		conn := testAccProvider.Meta().(*APIClient).conn
+		input := &v1.ListInput{
+			FQDN: gofastly.ToPointer(fqdn),
+		}
+		cl, err := v1.List(conn, input)
+		if err != nil {
+			return fmt.Errorf("failed to list domains for fastly_domain_v1 resource: %w", err)
+		}
+		if cl != nil && len(cl.Data) > 0 {
+			for _, d := range cl.Data {
+				if d.FQDN == fqdn {
+					return fmt.Errorf("tried deleting domain (%s), but was still found", fqdn)
+				}
+			}
+		}
+		return nil
+	}
+	return nil
+}

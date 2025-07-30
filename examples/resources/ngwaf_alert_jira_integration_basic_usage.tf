479e864980b5a9cb7a33b3c7b493db52c094524b
Anthony Gomez <anthony.gomez@fastly.com>
2025-07-30 11:29:46 -0400

    fix example

--- examples/resources/ngwaf_alert_jira_integration_basic_usage.tf
+++ examples/resources/ngwaf_alert_jira_integration_basic_usage.tf
@@ -1,6 +1,9 @@
 resource "fastly_ngwaf_alert_jira_integration" "demo_jira_alert" {
-  description    = "Some Description"
-  key            = "123456789"
-  site           = "us1"
+  description      = "A description"
+  host             = "https://mycompany.atlassian.net"
+  issue_type       = "task"
+  key              = "a1b2c3d4e5f6789012345678901234567"
+  project          = "test"
+  username         = "user"
   workspace_id   = fastly_ngwaf_workspace.demo.id
 }

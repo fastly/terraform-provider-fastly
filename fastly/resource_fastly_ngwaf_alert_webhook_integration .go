c9ff78cc4fa087519a7a21b0d8d97146dd58afde
Richard Carillo <rcarillo@fastly.com>
2025-07-30 12:18:00 -0400

    started changes

---
+++ fastly/resource_fastly_ngwaf_alert_webhook_integration .go
@@ -0,0 +1,160 @@
+package fastly
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+
+	gofastly "github.com/fastly/go-fastly/v11/fastly"
+	webhookAlerts "github.com/fastly/go-fastly/v11/fastly/ngwaf/v1/workspaces/alerts/webhook"
+)
+
+func resourceFastlyNGWAFAlertWebhookIntegration() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceFastlyNGWAFAlertWebhookIntegrationCreate,
+		ReadContext:   resourceFastlyNGWAFAlertWebhookIntegrationRead,
+		UpdateContext: resourceFastlyNGWAFAlertWebhookIntegrationUpdate,
+		DeleteContext: resourceFastlyNGWAFAlertWebhookIntegrationDelete,
+		Importer: &schema.ResourceImporter{
+			StateContext: resourceFastlyNGWAFAlertWebhookIntegrationImport,
+		},
+		Schema: map[string]*schema.Schema{
+			"description": {
+				Description: "User-submitted description of the alert",
+				Optional:    true,
+				Type:        schema.TypeString,
+			},
+			"webhook": {
+				Description:  "The Webhook URL.",
+				Required:     true,
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(9, 9),
+			},
+			"workspace_id": {
+				Description: "The id of the workspace this alert belongs to.",
+				Required:    true,
+				Type:        schema.TypeString,
+			},
+		},
+	}
+}
+
+func resourceFastlyNGWAFAlertWebhookIntegrationCreate(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
+	conn := meta.(*APIClient).conn
+
+	workspaceID := d.Get("workspace_id").(string)
+
+	i := webhookAlerts.CreateInput{
+		Config: &webhookAlerts.CreateConfig{
+			Webhook: gofastly.ToPointer(d.Get("webhook").(string)),
+		},
+		Description: gofastly.ToPointer(d.Get("description").(string)),
+		Events:      gofastly.ToPointer([]string{"flag"}),
+		WorkspaceID: gofastly.ToPointer(workspaceID),
+	}
+
+	log.Printf("[DEBUG] CREATE: NGWAF Webhook alert input: %#v", i)
+
+	alert, err := webhookAlerts.Create(ctx, conn, &i)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	log.Printf("[DEBUG] CREATE: NGWAF Webhook alert result: %#v", alert)
+
+	d.SetId(alert.ID)
+
+	return resourceFastlyNGWAFAlertWebhookIntegrationRead(ctx, d, meta)
+}
+
+func resourceFastlyNGWAFAlertWebhookIntegrationRead(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
+	conn := meta.(*APIClient).conn
+
+	workspaceID := d.Get("workspace_id").(string)
+
+	i := webhookAlerts.GetInput{
+		AlertID:     gofastly.ToPointer(d.Id()),
+		WorkspaceID: gofastly.ToPointer(workspaceID),
+	}
+
+	log.Printf("[DEBUG] REFRESH: NGWAF Webhook alert input: id=%s, workspaceID=%s", d.Id(), workspaceID)
+
+	alert, err := webhookAlerts.Get(gofastly.NewContextForResourceID(ctx, d.Id()), conn, &i)
+	if err != nil {
+		if e, ok := err.(*gofastly.HTTPError); ok && e.IsNotFound() {
+			log.Printf("[WARN] Webhook alert not found '%s'", d.Id())
+			d.SetId("")
+			return nil
+		}
+		return diag.FromErr(err)
+	}
+	if err := d.Set("description", alert.Description); err != nil {
+		return diag.FromErr(err)
+	}
+	if err := d.Set("webhook", alert.Config.Webhook); err != nil {
+		return diag.FromErr(err)
+	}
+
+	return nil
+}
+
+func resourceFastlyNGWAFAlertWebhookIntegrationUpdate(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
+	conn := meta.(*APIClient).conn
+
+	i := webhookAlerts.UpdateInput{
+		AlertID: gofastly.ToPointer(d.Id()),
+		Config: &webhookAlerts.UpdateConfig{
+			Webhook: gofastly.ToPointer(d.Get("webhook").(string)),
+		},
+		WorkspaceID: gofastly.ToPointer(d.Get("workspace_id").(string)),
+	}
+
+	log.Printf("[DEBUG] UPDATE: NGWAF Webhook alert input: %#v", i)
+
+	_, err := webhookAlerts.Update(gofastly.NewContextForResourceID(ctx, d.Id()), conn, &i)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	return resourceFastlyNGWAFAlertWebhookIntegrationRead(ctx, d, meta)
+}
+
+func resourceFastlyNGWAFAlertWebhookIntegrationDelete(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
+	conn := meta.(*APIClient).conn
+
+	workspaceID := d.Get("workspace_id").(string)
+
+	i := webhookAlerts.DeleteInput{
+		AlertID:     gofastly.ToPointer(d.Id()),
+		WorkspaceID: gofastly.ToPointer(workspaceID),
+	}
+
+	log.Printf("[DEBUG] DELETE: NGWAF Webhook alert input: id=%s, workspaceID=%s", d.Id(), workspaceID)
+
+	if err := webhookAlerts.Delete(gofastly.NewContextForResourceID(ctx, d.Id()), conn, &i); err != nil {
+		return diag.FromErr(err)
+	}
+
+	return nil
+}
+
+func resourceFastlyNGWAFAlertWebhookIntegrationImport(_ context.Context, d *schema.ResourceData, _ any) ([]*schema.ResourceData, error) {
+	log.Printf("[DEBUG] IMPORT: NGWAF Webhook alert ID: %s", d.Id())
+
+	workspaceID, alertWebhookIntegrationID, isInCorrectForm := strings.Cut(d.Id(), "/")
+	if !isInCorrectForm {
+		return nil, fmt.Errorf("invalid ID format: %s. Expected format: <workspaceID>/<alertID>", d.Id())
+	}
+
+	if err := d.Set("workspace_id", workspaceID); err != nil {
+		return nil, fmt.Errorf("error setting workspace_id (%s): %w", workspaceID, err)
+	}
+
+	d.SetId(alertWebhookIntegrationID)
+
+	return []*schema.ResourceData{d}, nil
+}

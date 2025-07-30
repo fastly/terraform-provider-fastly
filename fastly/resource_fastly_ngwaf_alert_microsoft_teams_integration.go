f4f6c10c8ada8453d02730503468fcd6f54f2d20
Anthony Gomez <anthony.gomez@fastly.com>
2025-07-30 16:44:46 -0400

    feat(ngwaf/alerts/microsoft-teams): adds support for microsoft teams alerts (#1045)

---
+++ fastly/resource_fastly_ngwaf_alert_microsoft_teams_integration.go
@@ -0,0 +1,158 @@
+package fastly
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	gofastly "github.com/fastly/go-fastly/v11/fastly"
+	microsoftTeamsAlerts "github.com/fastly/go-fastly/v11/fastly/ngwaf/v1/workspaces/alerts/microsoftteams"
+)
+
+func resourceFastlyNGWAFAlertMicrosoftTeamsIntegration() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceFastlyNGWAFAlertMicrosoftTeamsIntegrationCreate,
+		ReadContext:   resourceFastlyNGWAFAlertMicrosoftTeamsIntegrationRead,
+		UpdateContext: resourceFastlyNGWAFAlertMicrosoftTeamsIntegrationUpdate,
+		DeleteContext: resourceFastlyNGWAFAlertMicrosoftTeamsIntegrationDelete,
+		Importer: &schema.ResourceImporter{
+			StateContext: resourceFastlyNGWAFAlertMicrosoftTeamsIntegrationImport,
+		},
+		Schema: map[string]*schema.Schema{
+			"description": {
+				Description: "User-submitted description of the alert",
+				Optional:    true,
+				Type:        schema.TypeString,
+			},
+			"webhook": {
+				Description: "The Microsoft Teams webhook URL.",
+				Required:    true,
+				Type:        schema.TypeString,
+			},
+			"workspace_id": {
+				Description: "The id of the workspace this alert belongs to.",
+				Required:    true,
+				Type:        schema.TypeString,
+			},
+		},
+	}
+}
+
+func resourceFastlyNGWAFAlertMicrosoftTeamsIntegrationCreate(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
+	conn := meta.(*APIClient).conn
+
+	workspaceID := d.Get("workspace_id").(string)
+
+	i := microsoftTeamsAlerts.CreateInput{
+		Config: &microsoftTeamsAlerts.CreateConfig{
+			Webhook: gofastly.ToPointer(d.Get("webhook").(string)),
+		},
+		Description: gofastly.ToPointer(d.Get("description").(string)),
+		Events:      gofastly.ToPointer([]string{"flag"}),
+		WorkspaceID: gofastly.ToPointer(workspaceID),
+	}
+
+	log.Printf("[DEBUG] CREATE: NGWAF Microsoft Teams alert input: %#v", i)
+
+	alert, err := microsoftTeamsAlerts.Create(gofastly.NewContextForResourceID(ctx, workspaceID), conn, &i)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	log.Printf("[DEBUG] CREATE: NGWAF Microsoft Teams alert result: %#v", alert)
+
+	d.SetId(alert.ID)
+
+	return resourceFastlyNGWAFAlertMicrosoftTeamsIntegrationRead(ctx, d, meta)
+}
+
+func resourceFastlyNGWAFAlertMicrosoftTeamsIntegrationRead(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
+	conn := meta.(*APIClient).conn
+
+	workspaceID := d.Get("workspace_id").(string)
+
+	i := microsoftTeamsAlerts.GetInput{
+		AlertID:     gofastly.ToPointer(d.Id()),
+		WorkspaceID: gofastly.ToPointer(workspaceID),
+	}
+
+	log.Printf("[DEBUG] REFRESH: NGWAF Microsoft Teams alert input: id=%s, workspaceID=%s", d.Id(), workspaceID)
+
+	alert, err := microsoftTeamsAlerts.Get(gofastly.NewContextForResourceID(ctx, workspaceID), conn, &i)
+	if err != nil {
+		if e, ok := err.(*gofastly.HTTPError); ok && e.IsNotFound() {
+			log.Printf("[WARN] Microsoft Teams alert not found '%s'", d.Id())
+			d.SetId("")
+			return nil
+		}
+		return diag.FromErr(err)
+	}
+	if err := d.Set("description", alert.Description); err != nil {
+		return diag.FromErr(err)
+	}
+	if err := d.Set("webhook", alert.Config.Webhook); err != nil {
+		return diag.FromErr(err)
+	}
+
+	return nil
+}
+
+func resourceFastlyNGWAFAlertMicrosoftTeamsIntegrationUpdate(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
+	conn := meta.(*APIClient).conn
+
+	i := microsoftTeamsAlerts.UpdateInput{
+		AlertID: gofastly.ToPointer(d.Id()),
+		Config: &microsoftTeamsAlerts.UpdateConfig{
+			Webhook: gofastly.ToPointer(d.Get("webhook").(string)),
+		},
+		WorkspaceID: gofastly.ToPointer(d.Get("workspace_id").(string)),
+	}
+
+	log.Printf("[DEBUG] UPDATE: NGWAF Microsoft Teams alert input: %#v", i)
+
+	_, err := microsoftTeamsAlerts.Update(gofastly.NewContextForResourceID(ctx, d.Get("workspace_id").(string)), conn, &i)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	return resourceFastlyNGWAFAlertMicrosoftTeamsIntegrationRead(ctx, d, meta)
+}
+
+func resourceFastlyNGWAFAlertMicrosoftTeamsIntegrationDelete(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
+	conn := meta.(*APIClient).conn
+
+	workspaceID := d.Get("workspace_id").(string)
+
+	i := microsoftTeamsAlerts.DeleteInput{
+		AlertID:     gofastly.ToPointer(d.Id()),
+		WorkspaceID: gofastly.ToPointer(workspaceID),
+	}
+
+	log.Printf("[DEBUG] DELETE: NGWAF Microsoft	Teams alert input: id=%s, workspaceID=%s", d.Id(), workspaceID)
+
+	if err := microsoftTeamsAlerts.Delete(gofastly.NewContextForResourceID(ctx, workspaceID), conn, &i); err != nil {
+		return diag.FromErr(err)
+	}
+
+	return nil
+}
+
+func resourceFastlyNGWAFAlertMicrosoftTeamsIntegrationImport(_ context.Context, d *schema.ResourceData, _ any) ([]*schema.ResourceData, error) {
+	log.Printf("[DEBUG] IMPORT: NGWAF Microsoft Teams alert ID: %s", d.Id())
+
+	workspaceID, alertMicrosoftTeamsIntegrationID, isInCorrectForm := strings.Cut(d.Id(), "/")
+	if !isInCorrectForm {
+		return nil, fmt.Errorf("invalid ID format: %s. Expected format: <workspaceID>/<alertID>", d.Id())
+	}
+
+	if err := d.Set("workspace_id", workspaceID); err != nil {
+		return nil, fmt.Errorf("error setting workspace_id (%s): %w", workspaceID, err)
+	}
+
+	d.SetId(alertMicrosoftTeamsIntegrationID)
+
+	return []*schema.ResourceData{d}, nil
+}
---
+++ fastly/resource_fastly_ngwaf_alert_microsoft_teams_integration_test.go
@@ -0,0 +1,146 @@
+package fastly
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+
+	microsoftTeamsAlerts "github.com/fastly/go-fastly/v11/fastly/ngwaf/v1/workspaces/alerts/microsoftteams"
+)
+
+const (
+	AlertMicrosoftTeamsIntegrationDescription = "Test NGWAF Microsoft Teams Alert"
+	AlertMicrosoftTeamsIntegrationURL         = "https://example.com/microsoft-teams/my-service"
+	AlertMicrosoftTeamsIntegrationURLUpdated  = "https://example.com/microsoft-teams/my-service-2"
+)
+
+func TestAccFastlyNGWAFAlertMicrosoftTeamsIntegration_validate(t *testing.T) {
+	var (
+		alertMicrosoftTeamsIntegrationID string
+		workspaceID                      string
+	)
+	workspaceName := fmt.Sprintf("NGWAF Workspace %s", acctest.RandString(10))
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: testAccProviders,
+		CheckDestroy:      testAccCheckNGWAFAlertMicrosoftTeamsIntegrationDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNGWAFAlertMicrosoftTeamsIntegrationConfig(workspaceName, AlertMicrosoftTeamsIntegrationDescription, AlertMicrosoftTeamsIntegrationURL),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("fastly_ngwaf_alert_microsoft_teams_integration.test_microsoft_teams_alert", "description", AlertMicrosoftTeamsIntegrationDescription),
+					resource.TestCheckResourceAttr("fastly_ngwaf_alert_microsoft_teams_integration.test_microsoft_teams_alert", "webhook", AlertMicrosoftTeamsIntegrationURL),
+					testAccNGWAFAlertMicrosoftTeamsIntegrationExists("fastly_ngwaf_alert_microsoft_teams_integration.test_microsoft_teams_alert", "fastly_ngwaf_workspace.test_microsoft_teams_alert_workspace", &alertMicrosoftTeamsIntegrationID, &workspaceID),
+				),
+			},
+			{
+				Config: testAccNGWAFAlertMicrosoftTeamsIntegrationConfig(workspaceName, AlertMicrosoftTeamsIntegrationDescription, AlertMicrosoftTeamsIntegrationURLUpdated),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("fastly_ngwaf_alert_microsoft_teams_integration.test_microsoft_teams_alert", "description", AlertMicrosoftTeamsIntegrationDescription),
+					resource.TestCheckResourceAttr("fastly_ngwaf_alert_microsoft_teams_integration.test_microsoft_teams_alert", "webhook", AlertMicrosoftTeamsIntegrationURLUpdated),
+					testAccNGWAFAlertMicrosoftTeamsIntegrationExists("fastly_ngwaf_alert_microsoft_teams_integration.test_microsoft_teams_alert", "fastly_ngwaf_workspace.test_microsoft_teams_alert_workspace", &alertMicrosoftTeamsIntegrationID, &workspaceID),
+				),
+			},
+			{
+				ResourceName: "fastly_ngwaf_alert_microsoft_teams_integration.test_microsoft_teams_alert",
+				ImportStateIdFunc: func(_ *terraform.State) (string, error) {
+					log.Printf("[DEBUG] IMPORT TEST: NGWAF Microsoft Teams alert ID: %s", alertMicrosoftTeamsIntegrationID)
+					log.Printf("[DEBUG] IMPORT TEST: NGWAF workspace ID: %s", workspaceID)
+					return fmt.Sprintf("%s/%s", workspaceID, alertMicrosoftTeamsIntegrationID), nil
+				},
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccNGWAFAlertMicrosoftTeamsIntegrationExists(alertMicrosoftTeamsIntegrationName, workspaceName string, alertMicrosoftTeamsIntegrationID, workspaceID *string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[alertMicrosoftTeamsIntegrationName]
+		if !ok {
+			return fmt.Errorf("Not found: %s", alertMicrosoftTeamsIntegrationName)
+		}
+		ws, ok := s.RootModule().Resources[workspaceName]
+		if !ok {
+			return fmt.Errorf("Not found: %s", workspaceName)
+		}
+		rID := rs.Primary.ID
+		wID := ws.Primary.ID
+
+		conn := testAccProvider.Meta().(*APIClient).conn
+		latest, err := microsoftTeamsAlerts.Get(context.TODO(), conn, &microsoftTeamsAlerts.GetInput{
+			AlertID:     &rID,
+			WorkspaceID: &wID,
+		})
+		if err != nil {
+			return fmt.Errorf("Unable to retrieve NGWAF Microsoft Teams Alert %s: %v", rID, err)
+		}
+
+		if latest == nil {
+			return fmt.Errorf("NGWAF Microsoft Teams Alert %s not found in API", wID)
+		}
+
+		*alertMicrosoftTeamsIntegrationID = rID
+		*workspaceID = wID
+		log.Printf("[DEBUG] EXISTS IMPORT: NGWAF Microsoft Teams alert ID: %s", *alertMicrosoftTeamsIntegrationID)
+		log.Printf("[DEBUG] EXISTS IMPORT: NGWAF workspace ID: %s", *workspaceID)
+
+		return nil
+	}
+}
+
+func testAccCheckNGWAFAlertMicrosoftTeamsIntegrationDestroy(s *terraform.State) error {
+	conn := testAccProvider.Meta().(*APIClient).conn
+	var wsID string
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "fastly_ngwaf_workspace" {
+			continue
+		}
+
+		wsID = rs.Primary.ID
+	}
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "fastly_ngwaf_alert_microsoft_teams_integration" {
+			continue
+		}
+
+		_, err := microsoftTeamsAlerts.Get(context.TODO(), conn, &microsoftTeamsAlerts.GetInput{
+			AlertID:     &rs.Primary.ID,
+			WorkspaceID: &wsID,
+		})
+		if err == nil {
+			return fmt.Errorf("NGWAF MicrosoftTeams Alert %s still exists", rs.Primary.ID)
+		}
+	}
+	return nil
+}
+
+func testAccNGWAFAlertMicrosoftTeamsIntegrationConfig(workspaceName, description, webhook string) string {
+	return fmt.Sprintf(`
+resource "fastly_ngwaf_workspace" "test_microsoft_teams_alert_workspace" {
+  name                           = "%s"
+  description                    = "Test NGWAF Workspace"
+  mode                            = "block"
+
+  attack_signal_thresholds {
+    one_minute  = 100
+    ten_minutes = 500
+    one_hour    = 1000
+    immediate   = true
+  }
+}
+
+resource "fastly_ngwaf_alert_microsoft_teams_integration" "test_microsoft_teams_alert" {
+  description      = "%s"
+  webhook          = "%s"
+  workspace_id     = fastly_ngwaf_workspace.test_microsoft_teams_alert_workspace.id
+}
+`, workspaceName, description, webhook)
+}

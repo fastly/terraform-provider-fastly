56ca470003bc70888fe7606052637c7233046b55
Alex O <aorlov@fastly.com>
2025-08-29 16:36:57 +0200

    Fix the documented ngwaf_workspace example to make a a valid resource declaration

--- examples/resources/ngwaf_workspace_basic_usage.tf
+++ examples/resources/ngwaf_workspace_basic_usage.tf
@@ -1,5 +1,12 @@
 resource "fastly_ngwaf_workspace" "demo" {
   name = "demofastly"
 
-  force_destroy = true
+  description = "A reference setup"
+  mode        = "block"
+  attack_signal_thresholds {
+    immediate = true
+  }
+  default_blocking_response_code = 406
+  ip_anonymization               = null
+  client_ip_headers              = []
 }

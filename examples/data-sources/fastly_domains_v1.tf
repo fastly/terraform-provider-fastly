98d55f87431eca2c1dcd3b3d9f1331144103b48b
Richard Carillo <rcarillo@fastly.com>
2025-09-30 09:15:01 -0400

    Terminology update

--- examples/data-sources/fastly_domains_v1.tf
+++ examples/data-sources/fastly_domains_v1.tf
@@ -1,11 +1,10 @@
 data "fastly_domains_v1" "example" {
 }
 
-  output "all_domains" {
+output "all_domains" {
   value = data.fastly_domains_v1.example.domains
 }
 
 output "total_domains" {
   value = data.fastly_domains_v1.example.total
-  
 }

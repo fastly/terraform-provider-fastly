97ac0c180e6872ce34be793dd3cd73d044e94b9a
case-fastly <141883771+case-fastly@users.noreply.github.com>
2026-02-05 18:18:03 -0800

    Add a Data Source for Staging IPs

---
+++ fastly/data_source_staging_ips.go
@@ -0,0 +1,99 @@
+package fastly
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	gofastly "github.com/fastly/go-fastly/v12/fastly"
+
+	"github.com/fastly/terraform-provider-fastly/fastly/hashcode"
+)
+
+func dataSourceFastlyStagingIPs() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceFastlyStagingIPsRead,
+		Schema: map[string]*schema.Schema{
+			"domains": {
+				Type:        schema.TypeSet,
+				Computed:    true,
+				Description: "List of domains with their staging IP addresses.",
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "The domain name.",
+						},
+						"staging_ip": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "The staging IP address for the domain.",
+						},
+					},
+				},
+			},
+			"service_id": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "Alphanumeric string identifying the service.",
+			},
+			"service_version": {
+				Type:        schema.TypeInt,
+				Required:    true,
+				Description: "Integer identifying a service version.",
+			},
+		},
+	}
+}
+
+func dataSourceFastlyStagingIPsRead(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
+	conn := meta.(*APIClient).conn
+
+	log.Printf("[DEBUG] Reading staging IPs")
+
+	remoteState, err := conn.ListDomains(ctx, &gofastly.ListDomainsInput{
+		ServiceID:      d.Get("service_id").(string),
+		ServiceVersion: d.Get("service_version").(int),
+		Include:        "staging_ips",
+	})
+	if err != nil {
+		return diag.Errorf("error fetching staging IPs: %s", err)
+	}
+
+	hashBase, _ := json.Marshal(remoteState)
+	hashString := strconv.Itoa(hashcode.String(string(hashBase)))
+	d.SetId(hashString)
+
+	if err := d.Set("domains", flattenDataSourceStagingIPs(remoteState)); err != nil {
+		return diag.Errorf("error setting domains: %s", err)
+	}
+
+	return nil
+}
+
+// flattenDataSourceStagingIPs models data into format suitable for saving to
+// Terraform state.
+func flattenDataSourceStagingIPs(remoteState []*gofastly.Domain) []map[string]any {
+	result := make([]map[string]any, len(remoteState))
+	if len(remoteState) == 0 {
+		return result
+	}
+
+	for i, resource := range remoteState {
+		result[i] = map[string]any{}
+
+		if resource.Name != nil {
+			result[i]["name"] = *resource.Name
+		}
+		if resource.StagingIP != nil {
+			result[i]["staging_ip"] = *resource.StagingIP
+		}
+	}
+
+	return result
+}
---
+++ fastly/data_source_staging_ips_test.go
@@ -0,0 +1,67 @@
+package fastly
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+)
+
+func TestAccFastlyDataSourceStagingIPs_Config(t *testing.T) {
+	h := generateHex()
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		ProviderFactories: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccFastlyDataSourceStagingIPsConfig(h),
+				Check: resource.ComposeTestCheckFunc(
+					func(s *terraform.State) error {
+						r := s.RootModule().Resources["data.fastly_staging_ips.example"]
+						a := r.Primary.Attributes
+
+						domains, err := strconv.Atoi(a["domains.#"])
+						if err != nil {
+							return err
+						}
+
+						if domains < 1 {
+							return fmt.Errorf("expected at least one domain with a staging IP, got %d", domains)
+						}
+
+						return nil
+					},
+				),
+			},
+		},
+	})
+}
+
+func testAccFastlyDataSourceStagingIPsConfig(h string) string {
+	tf := `
+resource "fastly_service_vcl" "example" {
+  name = "tf_example_service_for_staging_ips_data_source"
+
+  domain {
+    name = "%s.com"
+  }
+
+  force_destroy = true
+
+  stage = true
+}
+
+data "fastly_staging_ips" "example" {
+  depends_on      = [fastly_service_vcl.example]
+  service_id      = fastly_service_vcl.example.id
+  service_version = fastly_service_vcl.example.staged_version
+}
+`
+
+	return fmt.Sprintf(tf, h)
+}

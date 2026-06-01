c7d52c3f8288cb6ea66051eafbb8f1e2cb8b03fe
Philipp Schulte <pschulte@fastly.com>
2026-06-01 09:21:09 -0400

    refactor(client): generalize Fastly error helper file

--- internal/client/not_found.go
+++
@@ -1,8 +0,0 @@
-package client
-
-import fastly "github.com/fastly/go-fastly/v15/fastly"
-
-func IsNotFound(err error) bool {
-	httpErr, ok := err.(*fastly.HTTPError)
-	return ok && httpErr.StatusCode == 404
-}

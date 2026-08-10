577c32d68c79b022366b8e2068dd80dee03a32dc
Richard Carillo <rcarillo@fastly.com>
2026-08-10 11:19:42 -0400

    fix(ngwaf/rules): corrected a behaviaor where rules with a deception action had their deception_type (and allow_interactive) fields omitted from update operations

--- fastly/ngwaf_rule_expand.go
+++ fastly/ngwaf_rule_expand.go
@@ -147,6 +147,12 @@ func expandNGWAFRuleUpdateActions(raw []any, scopeType string) []*rules.UpdateAc
 			action.Signal = gofastly.ToPointer(v.(string))
 		}
 		if scopeType == "workspace" {
+			if v, ok := m["allow_interactive"]; ok && v.(bool) {
+				action.AllowInteractive = gofastly.ToPointer(v.(bool))
+			}
+			if v, ok := m["deception_type"]; ok && v != "" {
+				action.DeceptionType = gofastly.ToPointer(v.(string))
+			}
 			if v, ok := m["redirect_url"]; ok {
 				action.RedirectURL = gofastly.ToPointer(v.(string))
 			}
